83befd31add96909c8c738792a5a419a4e72898f
Videnoff <ivan.videnov@hotmail.com>
2020-05-04 08:18:58 +0300

    Create 01.Hello-SoftUni.cs

---
+++ C# Web Developer/Programming Basics/01. First Steps In Coding/01.Hello-SoftUni.cs
@@ -0,0 +1,12 @@
+namespace P01_HelloSoftUni
+{
+    using System;
+
+    class P01_HelloSoftUni
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SoftUni");
+        }
+    }
+}
