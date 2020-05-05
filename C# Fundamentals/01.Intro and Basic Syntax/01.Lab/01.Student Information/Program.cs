8341d789146efcf90d9923b60fc792ddbc2e017c
Videnoff <ivan.videnov@hotmail.com>
2020-05-05 20:24:22 +0000

    Create Program.cs

---
+++ C# Fundamentals/01.Intro and Basic Syntax/01.Lab/01.Student Information/Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace StudentInformation
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            double grade = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}, Age: {age}, Grade: {grade:F2}");
+        }
+    }
+}
