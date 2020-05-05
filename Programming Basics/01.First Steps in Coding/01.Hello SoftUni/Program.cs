1a2ef5eb6b0831209644d8e83b8ffd2f0b7f6e5e
Videnoff <ivan.videnov@hotmail.com>
2020-05-05 19:44:59 +0000

    Add files via upload

---
+++ Programming Basics/01.First Steps in Coding/01.Hello SoftUni/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloSoftUni
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SoftUni");
+        }
+    }
+}
