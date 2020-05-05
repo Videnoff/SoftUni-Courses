75f4c17deb34ec0115bcd8c05198c26c27a5e886
Videnoff <ivan.videnov@hotmail.com>
2020-05-05 20:45:49 +0000

    Create Program.cs

---
+++ Programming Basics/02.Simple Operations and Calculations/01.Lab/01.Greeting by Name/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace GreetingbyName
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, {0}!", name);
+        }
+    }
+}
