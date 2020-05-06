d722d4ecb6cf53cca0195b2be322f945cd775eff
Videnoff <ivan.videnov@hotmail.com>
2020-05-06 18:03:42 +0300

    Create Program.cs

---
+++ C# Web Developer/C# Advanced/C# Advanced/03.Sets and Dictionaries Advanced/02.Exercises/01.Unique Usernames/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+namespace _01.Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var names = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+
+                names.Add(name);
+            }
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
