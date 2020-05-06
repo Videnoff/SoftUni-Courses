aab906bc39b79ceef048c3c19e31ec58b666791b
Videnoff <ivan.videnov@hotmail.com>
2020-05-06 20:45:00 +0300

    Add files via upload

---
+++ C# Web Developer/C# Advanced/C# Advanced/06.Defining Classes/01.Lab/01.Car/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        private int year;
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year
+        {
+            get { return this.year; }
+
+            set
+            {
+                if (this.year < 1989)
+                {
+                    throw new InvalidOperationException("Year cannot be less than 1989");
+                }
+
+                this.year = value;
+            }
+        }
+    }
+}
