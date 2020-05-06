9dc4b62f5b701592ead884146b543bf0d3cc05bc
Videnoff <ivan.videnov@hotmail.com>
2020-05-06 18:27:26 +0300

    Add files via upload

---
+++ C# Web Developer/C# Advanced/C# Advanced/04.Streams Files and Directories/01.Lab/06.Folder-Size/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace _06.Folder_Size
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var files = Directory.GetFiles(".");
+
+            var totalLength = 0m;
+
+            foreach (var file in files)
+            {
+                var fileInfo = new FileInfo(file);
+                totalLength += fileInfo.Length;
+            }
+
+            Console.WriteLine($"{totalLength / 1024 / 1024:F4} MB");
+        }
+    }
+}
---
+++ C# Web Developer/C# Advanced/C# Advanced/04.Streams Files and Directories/01.Lab/07.Print-Directories/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace _07.Print_Directories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Users\IvanV\source\repos\CSharp-Advanced-January2020";
+
+            PrintDirectory(path, string.Empty);
+        }
+
+        static void PrintDirectory(string path, string prefix)
+        {
+            var directories = Directory.GetDirectories(path);
+            var directoryInfo = new DirectoryInfo(path);
+
+            Console.WriteLine($"{prefix} Dir: {directoryInfo.Name}");
+
+            foreach (var directory in directories)
+            {
+                PrintDirectory(directory, prefix += "--");
+            }
+
+            var files = Directory.GetFiles(path);
+
+            foreach (var file in files)
+            {
+                var fileInfo = new FileInfo(file);
+
+                Console.WriteLine($"{prefix}-- File: {fileInfo.Name}");
+            }
+        }
+    }
+}
