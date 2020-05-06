eae3943490abc0a7f2ba8f45627c70483f6869c3
Videnoff <ivan.videnov@hotmail.com>
2020-05-06 11:51:13 +0300

    Add files via upload

---
+++ C# Web Developer/C# Advanced/C# Advanced/02.Multidimensional Arrays/01.Lab/03.Primary-Diagonal/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.Primary_Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            var matrix = new int[size, size];
+
+            for (int row = 0; row < size; row++)
+            {
+                var currentRow = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] += currentRow[col];
+                }
+            }
+
+            var diagonalSum = 0;
+
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size; col++)
+                {
+                    if (row == col)
+                    {
+                        diagonalSum += matrix[row, col];
+                    }
+                }
+            }
+
+            Console.WriteLine(diagonalSum);
+        }
+    }
+}
