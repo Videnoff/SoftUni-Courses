5aa8351b67bb8542cbc8e70d92b092bcfca89c1f
Videnoff <ivan.videnov@hotmail.com>
2020-05-07 22:23:51 +0300

    Add files via upload

---
+++ C# Web Developer/C# Advanced/C# Advanced/07.Workshop/01.Lab/01.Create Custom Data Structures/MyList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_Create_Custom_Data_Structures
+{
+    public class MyList
+    {
+
+        private int capacity;
+        private int[] data;
+
+        public MyList() : this(4)
+        {
+        }
+
+        public MyList(int capacity)
+        {
+            this.capacity = capacity;
+            this.data = new int[capacity];
+        }
+
+        public int Count { get; private set; }
+
+        public void Add(int number)
+        {
+            this.data[this.Count] = number;
+            this.Count++;
+        }
+
+        public void Clear()
+        {
+            this.Count = 0;
+            this.data = new int[capacity];
+        }
+    }
+}
