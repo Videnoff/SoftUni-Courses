f4cf68f3517d8ad6c59c6c221b5ca39fdd5907dc
Videnoff <ivan.videnov@hotmail.com>
2020-05-07 23:31:06 +0300

    Add files via upload

---
+++ C# Web Developer/C# Advanced/C# Advanced/09.Iterators and Comparators/02.Exercises/07.Custom-Linked-List/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _07.Custom_Linked_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
