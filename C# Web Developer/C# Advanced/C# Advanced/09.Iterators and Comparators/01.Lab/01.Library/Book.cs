1c7f6ebb8709a36aa3590a5ab2022a5e89db3551
Videnoff <ivan.videnov@hotmail.com>
2020-05-07 23:12:28 +0300

    Add files via upload

---
+++ C# Web Developer/C# Advanced/C# Advanced/09.Iterators and Comparators/01.Lab/01.Library/Book.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book
+    {
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Authors = authors;
+        }
+
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        public IReadOnlyList<string> Authors { get; set; }
+    }
+
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return this.books.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
