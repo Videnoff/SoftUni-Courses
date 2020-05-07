24cbe26e527d63fec098c650ed797bb9b909e570
Videnoff <ivan.videnov@hotmail.com>
2020-05-07 22:38:19 +0300

    Create StartUp.cs

---
+++ C# Web Developer/C# Advanced/C# Advanced/08.Generics/02.Exercises/01.Generic Box of String/StartUp.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace _01.Generic_Box_of_String
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string value = Console.ReadLine();
+
+                Box<string> box = new Box<string>(value);
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
