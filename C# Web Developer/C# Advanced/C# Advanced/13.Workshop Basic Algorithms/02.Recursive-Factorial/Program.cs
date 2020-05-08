fe10a7d3bd0bdd3952c5f88e65b0c202bed08933
Videnoff <ivan.videnov@hotmail.com>
2020-05-08 22:42:27 +0300

    Add files via upload

---
+++ C# Web Developer/C# Advanced/C# Advanced/13.Workshop Basic Algorithms/02.Recursive-Factorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02.Recursive_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var result = Factorial(n);
+
+            Console.WriteLine(result);
+        }
+
+        public static int Factorial(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+
+            return n * Factorial(n - 1);
+        }
+    }
+}
---
+++ C# Web Developer/C# Advanced/C# Advanced/13.Workshop Basic Algorithms/05.Merge-Sort/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _05.Merge_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
