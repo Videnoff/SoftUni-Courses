c8e646f102fd499feb5fe5883b245531331d9969
Videnoff <ivan.videnov@hotmail.com>
2020-05-08 22:56:36 +0300

    Add files via upload

---
+++ C# Web Developer/C# Advanced/C# Advanced/16.Exam Preparation 06/02.Excel-Functions/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Excel_Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            var table = new string[size][];
+
+            for (int row = 0; row < table.Length; row++)
+            {
+                var currentRow = Console.ReadLine().Split(", ");
+                table[row] = currentRow;
+
+                for (int col = 0; col < table[row].Length; col++)
+                {
+                    table[row][col] = currentRow[col];
+                }
+            }
+
+            var input = Console.ReadLine().Split();
+
+            var command = input[0];
+            var part = input[1];
+
+            var headerIndex = Array.IndexOf(table[0], part);
+
+            if (command == "hide")
+            {
+                for (int row = 0; row < table.Length; row++)
+                {
+                    List<string> lineToPrint = new List<string>(table[row]);
+
+                    lineToPrint.RemoveAt(headerIndex);
+
+                    Console.WriteLine(string.Join(" | ", lineToPrint));
+
+                    table[row] = lineToPrint.ToArray();
+                }
+            }
+            else if (command == "sort")
+            {
+                string[] headerRow = table[0];
+
+                Console.WriteLine(string.Join(" | ", headerRow));
+
+                table = table.OrderBy(x => x[headerIndex]).ToArray();
+
+                foreach (var row in table)
+                {
+                    if (row != headerRow)
+                    {
+                        Console.WriteLine(string.Join(" | ", row));
+                    }
+                }
+            }
+            else if (command == "filter")
+            {
+                var secondPart = input[2];
+
+                string[] headerRow = table[0];
+
+                Console.WriteLine(string.Join(" | ", headerRow));
+
+                for (int row = 0; row < table.Length; row++)
+                {
+                    if (table[row][headerIndex] == secondPart)
+                    {
+                        Console.WriteLine(string.Join(" | ", table[row]));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C# Web Developer/C# Advanced/C# Advanced/16.Exam Preparation 06/03.Repository/Repository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository
+{
+    public class Repository
+    {
+        Dictionary<int, Person> data;
+
+        private int id = -1;
+
+        public int Count => this.data.Count;
+
+        public Repository()
+        {
+            data = new Dictionary<int, Person>();
+        }
+
+        public void Add(Person person)
+        {
+            data.Add(++id, person);
+        }
+
+        public Person Get(int id)
+        {
+            foreach (var p in data)
+            {
+                if (p.Key == id)
+                {
+                    return p.Value;
+                }
+            }
+
+            return null;
+        }
+
+        public bool Update(int id, Person newPerson)
+        {
+            foreach (var person in data)
+            {
+                if (person.Key == id)
+                {
+                    data[id] = newPerson;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool Delete(int id)
+        {
+            foreach (var person in data)
+            {
+                if (person.Key == id)
+                {
+                    data.Remove(person.Key);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
