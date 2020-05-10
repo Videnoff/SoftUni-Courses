3d5b6094dd7c4e15c0d73effea9c0670e3b26a72
Videnoff <ivan.videnov@hotmail.com>
2020-05-10 11:56:21 +0300

    Add files via upload

---
+++ C# Web Developer/C# Advanced/C# OOP/02.Inheritance/01.Lab/05.Stack-of-Strings/StackOfStrings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05.Stack_of_Strings
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool isEmpty()
+        {
+            return this.Count == 0;
+        }
+
+        public void AddRange(IEnumerable<string> values)
+        {
+            foreach (var value in values)
+            {
+                this.Push(value);
+            }
+        }
+    }
+}
