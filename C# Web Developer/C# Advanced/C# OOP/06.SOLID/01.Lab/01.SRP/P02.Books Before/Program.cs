8ecc2afc8ce19f69dea0cc55f780c7bd26e56b78
Videnoff <ivan.videnov@hotmail.com>
2020-05-10 18:24:35 +0300

    Create Program.cs

---
+++ C# Web Developer/C# Advanced/C# OOP/06.SOLID/01.Lab/01.SRP/P02.Books Before/Program.cs
@@ -0,0 +1,12 @@
+namespace P02._Books_Before
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
