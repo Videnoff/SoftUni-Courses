a0bc37e51147663996566ff5c48552d98b8fd980
Videnoff <ivan.videnov@hotmail.com>
2020-05-10 12:07:17 +0300

    Add files via upload

---
+++ C# Web Developer/C# Advanced/C# OOP/03.Encapsulation/01.Lab/01.Persons/Person.cs
@@ -0,0 +1,29 @@
+﻿namespace PersonsInfo
+{
+    public class Person
+    {
+
+        //public Person()
+        //{
+            
+        //}
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        public string FirstName { get; internal set; }
+
+        public string LastName { get; internal set; }
+
+        public int Age { get; internal set; }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} is {this.Age} years old";
+        }
+    }
+}
