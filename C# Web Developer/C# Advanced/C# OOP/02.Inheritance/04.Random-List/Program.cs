0069fe6c9396a4bfe20f142a951497caeae64312
Videnoff <ivan.videnov@hotmail.com>
2020-05-10 11:51:24 +0300

    Add files via upload

---
+++ C# Web Developer/C# Advanced/C# OOP/02.Inheritance/04.Random-List/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _04.Random_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                var random = new Random();
+
+                Console.WriteLine(random.Next(0, 100));
+            }
+        }
+    }
+}
