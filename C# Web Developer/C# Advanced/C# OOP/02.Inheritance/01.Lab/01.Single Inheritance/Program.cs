a943b7bbe9ac38f1968f5adcf1e7b1754499428c
Videnoff <ivan.videnov@hotmail.com>
2020-05-10 11:48:09 +0300

    Create Program.cs

---
+++ C# Web Developer/C# Advanced/C# OOP/02.Inheritance/01.Lab/01.Single Inheritance/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace _01.Single_Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
