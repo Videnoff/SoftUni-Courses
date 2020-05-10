420e9bb46e04eddde518b681ef7276cc1c71f8aa
Videnoff <ivan.videnov@hotmail.com>
2020-05-10 12:04:33 +0300

    Add files via upload

---
+++ C# Web Developer/C# Advanced/C# OOP/02.Inheritance/02.Exercises/06.Animals/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Engine engine = new Engine();
+                engine.Run();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
