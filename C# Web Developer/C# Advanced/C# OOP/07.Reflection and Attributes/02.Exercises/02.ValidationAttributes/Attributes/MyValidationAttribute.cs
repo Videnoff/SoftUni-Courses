a93e5c10681d0856cdf20d0f486101330484297a
Videnoff <ivan.videnov@hotmail.com>
2020-05-12 09:48:56 +0300

    Add files via upload

---
+++ C# Web Developer/C# Advanced/C# OOP/07.Reflection and Attributes/02.Exercises/02.ValidationAttributes/Attributes/MyValidationAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ValidationAttributes.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+
+    public abstract class MyValidationAttribute : Attribute
+    {
+        /// <summary>
+        /// Accepts property value and returns boolean dependent on it's validity
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+
+        public abstract bool IsValid(object obj);
+    }
+}
---
+++ C# Web Developer/C# Advanced/C# OOP/07.Reflection and Attributes/02.Exercises/02.ValidationAttributes/Utilities/Validator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using ValidationAttributes.Attributes;
+
+namespace ValidationAttributes.Utilities
+{
+    public static class Validator
+    {
+        /// <summary>
+        /// Checks all object's properties for custom attributes and if all custom attributes are valid, then the whole object is valid
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+
+        public static bool IsValid(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            Type objType = obj.GetType();
+
+            PropertyInfo[] properties = objType.GetProperties();
+
+            /*
+             * If all properties are valid with their custom attributes -> Object is Valid
+             */
+            /*
+             * If one property is not valid for one of it's custom attributes -> Object is not Valid
+             */
+            foreach (var propertyInfo in properties)
+            {
+                MyValidationAttribute[] attributes = propertyInfo
+                    .GetCustomAttributes()
+                    .Where(ca => ca is MyValidationAttribute)
+                    .Cast<MyValidationAttribute>()
+                    .ToArray();
+
+                foreach (var attribute in attributes)
+                {
+                    if (!attribute.IsValid(propertyInfo.GetValue(obj)))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
