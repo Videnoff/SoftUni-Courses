62f0615e545e214b5431d37242131730822c78b1
Videnoff <ivan.videnov@hotmail.com>
2020-05-13 11:37:39 +0300

    Add files via upload

---
+++ C# Web Developer/C# Advanced/C# OOP/12.Design Patterns/02.Exercises/CompositePattern/CompositeGift.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CompositePattern
+{
+    public class CompositeGift : GiftBase, IGiftOperations
+    {
+        private readonly ICollection<GiftBase> gifts;
+
+        public CompositeGift(string name, int price) 
+            : base(name, price)
+        {
+            this.gifts = new List<GiftBase>();
+        }
+
+        public override int CalculateTotalPrice()
+        {
+            var totalPrice = 0;
+
+            // Output
+            Console.WriteLine($"{this.name} contains following products with prices:");
+
+            foreach (var gift in this.gifts)
+            {
+                totalPrice += gift.CalculateTotalPrice();
+            }
+
+            return totalPrice;
+        }
+
+        public void Add(GiftBase gift)
+        {
+            this.gifts.Add(gift);
+        }
+
+        public void Remove(GiftBase gift)
+        {
+            this.gifts.Remove(gift);
+        }
+    }
+}
