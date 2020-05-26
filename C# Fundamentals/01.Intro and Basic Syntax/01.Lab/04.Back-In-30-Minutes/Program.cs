4e931d4f2ec48d6b4fd06a93d6944cbe35b2801f
Videnoff <ivan.videnov@hotmail.com>
2020-05-26 17:31:00 +0300

    Add files via upload

---
+++ C# Fundamentals/01.Intro and Basic Syntax/01.Lab/04.Back-In-30-Minutes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BackIn30Minutes
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            int hoursInMins = hours * 60;
+            int totalTime = hoursInMins + minutes;
+            int timeAfterThirty = totalTime + 30;
+            int afterHours = timeAfterThirty / 60;
+            int afterMins = timeAfterThirty % 60;
+
+            if (afterHours > 23)
+            {
+                afterHours = afterHours - 24;
+            }
+            if (afterMins > 60)
+            {
+                afterHours++;
+            }
+            Console.WriteLine($"{afterHours}:{afterMins:D2}");
+        }
+    }
+}
