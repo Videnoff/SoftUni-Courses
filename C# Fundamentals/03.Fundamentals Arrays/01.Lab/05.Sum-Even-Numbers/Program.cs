021d497c3fb304feec266e5edecd3eb26e2b82b4
Videnoff <ivan.videnov@hotmail.com>
2020-05-29 21:57:32 +0300

    Add files via upload

---
+++ C# Fundamentals/03.Fundamentals Arrays/01.Lab/05.Sum-Even-Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace SumEvenNumbers
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int evens = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var num = nums[i];
+                if (num % 2 == 0)
+                {
+                    evens += num;
+                }
+            }
+            Console.WriteLine(evens);
+        }
+    }
+}
---
+++ C# Fundamentals/03.Fundamentals Arrays/01.Lab/06.Even-and-Odd-Subtraction/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+namespace EvenandOddSubtraction
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int evens = 0;
+            int odds = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var num = nums[i];
+                if (num % 2 == 0)
+                {
+                    evens += num;
+                }
+                else
+                {
+                    odds += num;
+                }
+            }
+            int diff = evens - odds;
+            Console.WriteLine(diff);
+        }
+    }
+}
