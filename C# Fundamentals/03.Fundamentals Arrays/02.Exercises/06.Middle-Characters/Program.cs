997001dec554f6f03293f01cea38e57a037167ce
Videnoff <ivan.videnov@hotmail.com>
2020-05-29 22:03:02 +0300

    Add files via upload

---
+++ C# Fundamentals/03.Fundamentals Arrays/02.Exercises/06.Middle-Characters/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiddleCharacters
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
