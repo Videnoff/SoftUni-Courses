6edcceaf7c0fc7db823db5fda4dad9b9cdec45e2
Videnoff <ivan.videnov@hotmail.com>
2020-05-29 22:01:19 +0300

    Add files via upload

---
+++ C# Fundamentals/03.Fundamentals Arrays/02.Exercises/02.Vowels-Count/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace VowelsCount
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string word = Console.ReadLine().ToLower();
+            int counter = 0;
+            counter = GetVoewls(word, counter);
+            Console.WriteLine(counter);
+        }
+
+        private static int GetVoewls(string word, int counter)
+        {
+            string vowels = "aouei";
+            //char[] vowels = { 'a', 'o', 'u', 'e', 'i'};
+            for (int i = 0; i < word.Length; i++)
+            {
+                char currentSymbol = word[i];
+                if (vowels.Contains(currentSymbol))
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+    }
+}
---
+++ C# Fundamentals/03.Fundamentals Arrays/02.Exercises/04.Password-Validator/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace PasswordValidator
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+            bool isLengthValid = IsLengthValid(password);
+            bool areSymbolsValid = AreSymbolsValid(password);
+            bool containsAtLeastTwoDigits = ContainsAtLeastTwoDigits(password);
+
+            PrintResult(isLengthValid, areSymbolsValid, containsAtLeastTwoDigits);
+        }
+
+        private static void PrintResult(bool isLengthValid, bool areSymbolsValid, bool containsAtLeastTwoDigits)
+        {
+            if (!isLengthValid)
+            {
+                Console.WriteLine("Password must be between 6 and 10 characters");
+            }
+            if (!areSymbolsValid)
+            {
+                Console.WriteLine("Password must consist only of letters and digits");
+            }
+            if (!containsAtLeastTwoDigits)
+            {
+                Console.WriteLine("Password must have at least 2 digits");
+            }
+            if (isLengthValid && areSymbolsValid && containsAtLeastTwoDigits)
+            {
+                Console.WriteLine("Password is valid");
+            }
+        }
+
+        private static bool ContainsAtLeastTwoDigits(string password)
+        {
+            int counter = 0;
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (char.IsDigit(password[i]))
+                {
+                    counter++;
+                    if (counter == 2)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private static bool AreSymbolsValid(string password)
+        {
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (!char.IsLetterOrDigit(password[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static bool IsLengthValid(string password)
+        {
+            if (password.Length >= 6 && password.Length <= 10)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
