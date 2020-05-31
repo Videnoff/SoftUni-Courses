c65b0e7e233b2226e5690f16a5cbfb52b2fc7cb0
Videnoff <ivan.videnov@hotmail.com>
2020-05-31 21:05:43 +0300

    Add files via upload

---
+++ C# Fundamentals/04.Methods/02.Exercises/01.Smallest-of-Three-Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SmallestofThreeNumbers
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Result();
+        }
+        static void Result()
+        {
+            int result = int.Parse(Console.ReadLine());
+            for (int i = 0; i < 2; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                result = SmallerNumber(number, result);
+            }
+            Console.WriteLine(result);
+        }
+        static int SmallerNumber(int result, int number)
+        {
+            if (result > number)
+            {
+                return number;
+            }
+            return result;
+        }
+    }
+}
+
