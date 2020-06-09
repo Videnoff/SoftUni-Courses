56dd8bf987069369185ee8b9d53601efd5899d9f
Videnoff <ivan.videnov@hotmail.com>
2020-06-09 23:11:41 +0300

    Add files via upload

---
+++ C# Fundamentals/09.Regular Expressions Regex/01.Lab/04.Reverse-Array-of-Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseArrayofStrings
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string[] texts = Console.ReadLine().Split();
+            for (int i = 0; i < texts.Length / 2; i++)
+            {
+                string first = texts[i];
+                string last = texts[texts.Length - i - 1];
+
+                texts[i] = last;
+                texts[texts.Length - i - 1] = first;
+            }
+            for (int i = 0; i < texts.Length; i++)
+            {
+                Console.Write(texts[i] + " ");
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/09.Regular Expressions Regex/01.Lab/07.Equal-Arrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace EqualArrays
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] firstArr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] secondArr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = 0;
+            bool findDiff = false;
+            for (int i = 0; i < firstArr.Length; i++)
+            {
+                if (firstArr[i] != secondArr[i])
+                {
+                    findDiff = true;
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    break;
+                }
+                else
+                {
+                    sum += firstArr[i];
+                }
+            }
+            if (findDiff != true)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            }
+        }
+    }
+}
