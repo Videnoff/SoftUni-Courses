687f477cbd70c5545b37979702e461034dc75594
Videnoff <ivan.videnov@hotmail.com>
2020-09-02 21:37:52 +0300

    Add files via upload

---
+++ C# Web Developer/C# DB/02.Entity-Framework-Core/01.ADO-NET-Exercises/P02.Villain-Names/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace P02.Villain_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(@"Server=DESKTOP-FSQK3RB;Database=MinionsDB;Integrated Security=true"))
+            {
+                sqlConnection.Open();
+                var select = "SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount " +
+                             "FROM Villains AS v " +
+                             "JOIN MinionsVillains AS mv ON v.Id = mv.VillainId " +
+                             "GROUP BY v.Id, v.Name " +
+                             "HAVING COUNT(mv.VillainId) > 3 " +
+                             "ORDER BY COUNT(mv.VillainId)";
+
+                SqlCommand command = new SqlCommand(select, sqlConnection);
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["Name"]} - {reader["MinionsCount"]}");
+                    }
+                }
+            }
+        }
+    }
+}
