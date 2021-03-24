c2a5b8eff2e0254ff07717da3e463d63c1ade5de
Videnoff <ivan.videnov@hotmail.com>
2021-03-24 19:41:36 +0200

    Add files via upload

---
+++ JS Advanced/01.JS Advanced/02.Arrays-and-Nested-Arrays/01.Lab/06.Bigger-Half.js
@@ -0,0 +1,7 @@
+function solve(arr) {
+    arr.sort((a, b) => a - b);
+    return arr.splice(Math.floor(arr.length / 2));
+}
+
+console.log(solve([4, 7, 2, 5]));
+console.log(solve([3, 19, 14, 7, 2, 19, 6]));
