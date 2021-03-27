817baed5be3d17d2d14083e97da6006cdf84f3a0
Videnoff <ivan.videnov@hotmail.com>
2021-03-27 12:28:59 +0200

    Add files via upload

---
+++ JS Advanced/01.JS Advanced/02.Arrays-and-Nested-Arrays/02.Exercise/02.Print-Every-N-th-Element-from-an-Array.js
@@ -0,0 +1,32 @@
+function solve(arr, step) {
+    let newArr = [];
+
+    for (let i = 0; i < arr.length; i += step) {
+        newArr.push(arr[i]);
+    }
+
+    return newArr;
+}
+
+console.log(solve(['5',
+        '20',
+        '31',
+        '4',
+        '20'],
+    2
+));
+
+console.log(solve(['dsa',
+        'asd',
+        'test',
+        'tset'],
+    2
+));
+
+console.log(solve(['1',
+        '2',
+        '3',
+        '4',
+        '5'],
+    6
+));
