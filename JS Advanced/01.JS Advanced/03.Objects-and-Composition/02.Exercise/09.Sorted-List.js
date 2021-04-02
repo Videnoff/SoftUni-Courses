75b75b6c89b6e1a5c2bb955b44ecff91d0fe8ef9
Videnoff <ivan.videnov@hotmail.com>
2021-04-02 20:41:41 +0300

    Add files via upload

---
+++ JS Advanced/01.JS Advanced/03.Objects-and-Composition/02.Exercise/09.Sorted-List.js
@@ -0,0 +1,41 @@
+function createSortedList() {
+    let list = [];
+
+    function add(el) {
+        list.push(el);
+        this.size++;
+        list.sort((a, b) => a - b);
+    }
+
+    function remove(index) {
+        if (index >= 0 && index < list.length) {
+            list.splice(index, 1);
+            this.size--;
+        }
+    }
+
+    function get(index) {
+        if (index >= 0 && index < list.length) {
+            return list[index];
+        }
+    }
+
+    function size() {
+        return list.length;
+    }
+
+    return {
+        size: 0,
+        add,
+        remove,
+        get
+    }
+}
+
+let list = createSortedList();
+list.add(5);
+list.add(6);
+list.add(7);
+console.log(list.get(1));
+list.remove(1);
+console.log(list.get(1));
