07ae410ccc32708e987c93dc5d7e9ba74b8f6a10
Videnoff <ivan.videnov@hotmail.com>
2021-04-08 20:24:13 +0300

    Add files via upload

---
+++ Programming Basics/02.Simple Operations and Calculations/01.Lab/02.Concatenate-Data/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConcatenateData
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            int years = int.Parse(Console.ReadLine());
+            string town = Console.ReadLine();
+
+            Console.WriteLine("You are {0} {1}, a {2}-years old person from {3}.", firstName, lastName, years, town);
+        }
+    }
+}
