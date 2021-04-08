b319b74805f8c45db6c806b3c0b9cab954107554
Videnoff <ivan.videnov@hotmail.com>
2021-04-08 20:12:23 +0300

    Add files via upload

---
+++ JS Advanced/01.JS Advanced/05.JS-Advanced-DOM-Events/02.Exercise/03. Locked-Profile/app.js
@@ -0,0 +1,19 @@
+function lockedProfile() {
+    let main = document.querySelector('#main');
+
+    main.addEventListener('click', (e) => {
+        if (e.target.tagName === 'BUTTON') {
+            const profile = e.target.parentNode;
+            const isLocked = profile.querySelector('input[type=radio]:checked').value === 'lock';
+
+            if (isLocked) {
+                return;
+            }
+
+            let div = profile.querySelector('div');
+            let isVisible = div.style.display === 'block';
+            div.style.display = isVisible ? 'none' : 'block';
+            e.target.textContent = !isVisible ? 'Hide it' : 'Show more';
+        }
+    })
+}
