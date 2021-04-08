0b7752b4860ab99e4b6e7fc6ac36c0428ad1d9e7
Videnoff <ivan.videnov@hotmail.com>
2021-04-08 20:21:59 +0300

    Add files via upload

---
+++ Programming Basics/01.First Steps in Coding/04.Rectangle-Area/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RectangleArea
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a * b);
+        }
+    }
+}
