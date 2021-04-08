8fb48d21d3c04d8855a6266b0366e15cc9ba8d83
Videnoff <ivan.videnov@hotmail.com>
2021-04-08 20:25:07 +0300

    Add files via upload

---
+++ Programming Basics/02.Simple Operations and Calculations/02.Exercise/02.Radians-to-Degrees/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RadianstoDegrees
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double rads = double.Parse(Console.ReadLine());
+            double grads = rads * 180 / Math.PI;
+            Console.WriteLine(Math.Round(grads));
+        }
+    }
+}
