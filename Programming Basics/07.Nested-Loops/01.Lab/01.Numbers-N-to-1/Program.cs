e44378dac168fb62c3ff4e3cacf0055ff27c13f1
Videnoff <ivan.videnov@hotmail.com>
2021-04-10 18:51:19 +0300

    Create Program.cs

---
+++ Programming Basics/07.Nested-Loops/01.Lab/01.Numbers-N-to-1/Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace NumbersNto1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+
+            for (int i = n; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
