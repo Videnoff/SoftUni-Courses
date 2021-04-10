6285c7b76210931545880d4a63c6f1a98ea775df
Videnoff <ivan.videnov@hotmail.com>
2021-04-10 18:57:30 +0300

    Add files via upload

---
+++ Programming Basics/08.PB-Online-Exam-9-and-10-March-2019/02.Football-Results/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace FootballResults
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string firstMatch = Console.ReadLine();
+            string secondMatch = Console.ReadLine();
+            string thirdMatch = Console.ReadLine();
+
+            int first = firstMatch.Length;
+            int second = secondMatch.Length;
+            int third = thirdMatch.Length;
+
+            int wins = 0;
+            int loses = 0;
+            int draws = 0;
+
+
+            if (firstMatch[0] > firstMatch[2])
+            {
+                wins++;
+            }
+            else if (firstMatch[0] < firstMatch[2])
+            {
+                loses++;
+            }
+            else if (firstMatch[0] == firstMatch[2])
+            {
+                draws++;
+            }
+
+            if (secondMatch[0] > secondMatch[2])
+            {
+                wins++;
+            }
+            else if (secondMatch[0] < secondMatch[2])
+            {
+                loses++;
+            }
+            else if (secondMatch[0] == secondMatch[2])
+            {
+                draws++;
+            }
+
+            if (thirdMatch[0] > thirdMatch[2])
+            {
+                wins++;
+            }
+            else if (thirdMatch[0] < thirdMatch[2])
+            {
+                loses++;
+            }
+            else if (thirdMatch[0] == thirdMatch[2])
+            {
+                draws++;
+            }
+
+            Console.WriteLine($"Team won {wins} games.");
+            Console.WriteLine($"Team lost {loses} games.");
+            Console.WriteLine($"Drawn games: {draws}");
+        }
+    }
+}
