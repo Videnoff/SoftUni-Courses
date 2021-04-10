ed23a3567071d9312942d4edeadd8c7728a48d84
Videnoff <ivan.videnov@hotmail.com>
2021-04-10 18:50:09 +0300

    Create Program.cs

---
+++ Programming Basics/06.For-Loop/02.Exercise/01.Numbers-Ending-in-7/Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace NumbersEndingin7
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 10 == 7)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
