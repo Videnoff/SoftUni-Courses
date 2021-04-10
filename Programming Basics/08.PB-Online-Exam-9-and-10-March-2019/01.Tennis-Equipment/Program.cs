023ea95502ae493ebac50a39f0007af24b47a3e7
Videnoff <ivan.videnov@hotmail.com>
2021-04-10 18:56:31 +0300

    Add files via upload

---
+++ Programming Basics/08.PB-Online-Exam-9-and-10-March-2019/01.Tennis-Equipment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TennisEquipment
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double tennisRacketPrice = double.Parse(Console.ReadLine());
+            double tennisRacketsCount = double.Parse(Console.ReadLine());
+            double sneakers = double.Parse(Console.ReadLine());
+
+            double racketsValue = tennisRacketPrice * tennisRacketsCount;
+            double sneakersPrice = sneakers * (tennisRacketPrice / 6.0);
+            double totalValue = racketsValue + sneakersPrice;
+            double equipment = (totalValue * 20) / 100;
+            double totalMoney = totalValue + equipment;
+            double jokoMoney = totalMoney / 8.0;
+            double sponsorsMoney = totalMoney - jokoMoney;
+
+            Console.WriteLine($"Price to be paid by Djokovic {Math.Floor(jokoMoney)}");
+            Console.WriteLine($"Price to be paid by sponsors {Math.Ceiling(sponsorsMoney)}");
+        }
+    }
+}
