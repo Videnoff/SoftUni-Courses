dae0f58df76f9734940aefcec5784c663cb2e4b6
Videnoff <ivan.videnov@hotmail.com>
2021-04-10 18:40:24 +0300

    Create Program.cs

---
+++ Programming Basics/03.Conditional-Statements/01.Lab/01.Excellent-Result/Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace ExcellentResult
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+            if (grade >= 5.50)
+            {
+                Console.WriteLine("Excellent!");
+            }
+        }
+    }
+}
