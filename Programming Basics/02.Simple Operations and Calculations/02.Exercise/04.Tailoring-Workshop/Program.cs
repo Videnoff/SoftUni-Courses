b3d3a4c4043b78a235944c207d21d00e0b6476b7
Videnoff <ivan.videnov@hotmail.com>
2021-04-11 23:27:58 +0300

    Add files via upload

---
+++ Programming Basics/02.Simple Operations and Calculations/02.Exercise/04.Tailoring-Workshop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TailoringWorkshop
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int tables = int.Parse(Console.ReadLine());
+            double tablesLength = double.Parse(Console.ReadLine());
+            double tablesWidth = double.Parse(Console.ReadLine());
+
+            double coverArea = tables * (tablesLength + (2 * 0.30)) * (tablesWidth + (2 * 0.30));
+            double squareArea = tables * (tablesLength / 2) * (tablesLength / 2);
+
+            double coverPriceUSD = coverArea * 7;
+            double squarePriceUSD = squareArea * 9;
+
+            double coverPriceBGN = coverPriceUSD * 1.85;
+            double squarePriceBGN = squarePriceUSD * 1.85;
+
+            double totalPriceUSD = coverPriceUSD + squarePriceUSD;
+            double totalPriceBGN = coverPriceBGN + squarePriceBGN;
+
+            Console.WriteLine("{0:F2} USD", totalPriceUSD);
+            Console.WriteLine("{0:F2} BGN", totalPriceBGN);
+        }
+    }
+}
