44c4c69f36a05af8e3647d410a75107e555da927
Videnoff <ivan.videnov@hotmail.com>
2021-04-11 23:27:10 +0300

    Add files via upload

---
+++ Programming Basics/02.Simple Operations and Calculations/02.Exercise/03.2D-Rectangle-Area/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DRectangleArea
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            double area = (Math.Abs(x1 - x2) * (Math.Abs(y1 - y2)));
+            double perimeter = Math.Abs((x1 - x2) * 2) + Math.Abs((y1 - y2) * 2);
+            Console.WriteLine("{0:F2}", area);
+            Console.WriteLine("{0:F2}", perimeter);
+        }
+    }
+}
