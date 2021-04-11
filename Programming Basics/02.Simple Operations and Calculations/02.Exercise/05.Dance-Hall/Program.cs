612d5b149c59571ed8f64b14bdfd13936003450a
Videnoff <ivan.videnov@hotmail.com>
2021-04-11 23:30:05 +0300

    Add files via upload

---
+++ Programming Basics/02.Simple Operations and Calculations/02.Exercise/05.Dance-Hall/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DanceHall
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double hallLength = double.Parse(Console.ReadLine());
+            double hallWidth = double.Parse(Console.ReadLine());
+            double wardrobeSide = double.Parse(Console.ReadLine());
+
+            double hallSize = (hallLength * 100) * (hallWidth * 100);
+            double benchSize = hallSize / 10;
+            double wardrobeSize = (wardrobeSide * 100) * (wardrobeSide * 100);
+            double clearArea = hallSize - benchSize - wardrobeSize;
+            double dancerPlace = clearArea / 7040;
+
+            Console.WriteLine(Math.Floor(dancerPlace));
+        }
+    }
+}
