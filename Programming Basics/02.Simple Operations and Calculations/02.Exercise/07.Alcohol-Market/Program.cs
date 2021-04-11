98c209eaebade5339f0a5da2dbe894954f465707
Videnoff <ivan.videnov@hotmail.com>
2021-04-11 23:37:19 +0300

    Add files via upload

---
+++ Programming Basics/02.Simple Operations and Calculations/02.Exercise/07.Alcohol-Market/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AlcoholMarket
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double whiskeyPrice = double.Parse(Console.ReadLine());
+            double beerLitters = double.Parse(Console.ReadLine());
+            double wineLitters = double.Parse(Console.ReadLine());
+            double rakijaLitters = double.Parse(Console.ReadLine());
+            double whiskeyLitters = double.Parse(Console.ReadLine());
+
+            double rakijaPrice = whiskeyPrice / 2;
+            double winePrice = (rakijaPrice - (rakijaPrice * 40) / 100);
+            double beerPrice = (rakijaPrice - (rakijaPrice * 80) / 100);
+
+            double sum = (whiskeyLitters * whiskeyPrice) + (beerLitters * beerPrice) + (wineLitters * winePrice) + (rakijaLitters * rakijaPrice);
+            Console.WriteLine($"{sum:F2}");
+        }
+    }
+}
