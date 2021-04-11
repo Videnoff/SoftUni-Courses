a1dfcc9c358fbe1f2bcd989bc9bed54e37a27b2f
Videnoff <ivan.videnov@hotmail.com>
2021-04-11 23:43:06 +0300

    Add files via upload

---
+++ Programming Basics/02.Simple Operations and Calculations/01.Lab/03.Square-Area/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SquareArea
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine(a * a);
+        }
+    }
+}
