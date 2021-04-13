4d5b7fdd15ff07a057f87a29d20433f922f961cf
Videnoff <ivan.videnov@hotmail.com>
2021-04-13 20:39:37 +0000

    Add files via upload

---
+++ Programming Basics/02.Simple Operations and Calculations/01.Lab/09.Birthday/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Birthday
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+
+            double volume = length * width * height;
+            double litters = volume * 0.001;
+            double sandPercent = percent * 0.01;
+            double waterLitters = litters * (1 - sandPercent);
+
+
+            Console.WriteLine("{0:F3}" ,waterLitters);
+        }
+    }
+}
