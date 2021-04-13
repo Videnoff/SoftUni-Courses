857ef88aa5e6202a650d46c3ad0554a37cc2ef97
Videnoff <ivan.videnov@hotmail.com>
2021-04-13 20:30:02 +0000

    Add files via upload

---
+++ Programming Basics/02.Simple Operations and Calculations/01.Lab/04.Inches-to-Centimeters/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace InchestoCentimeters
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double inches = double.Parse(Console.ReadLine());
+            double centimeters = inches * 2.54;
+            Console.WriteLine("{0:F2}", centimeters);
+        }
+    }
+}
