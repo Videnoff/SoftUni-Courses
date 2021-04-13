f5a4c988b9a2f81b25dbd83b42a1dee7834d1d0f
Videnoff <ivan.videnov@hotmail.com>
2021-04-13 20:38:54 +0000

    Add files via upload

---
+++ Programming Basics/02.Simple Operations and Calculations/01.Lab/08.Celsius-to-Fahrenheit/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CelsiustoFahrenheit
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double celcius = double.Parse(Console.ReadLine());
+            double fahrenheit = (celcius * 9) / 5 + 32;
+            Console.WriteLine("{0:F2}", fahrenheit);
+        }
+    }
+}
