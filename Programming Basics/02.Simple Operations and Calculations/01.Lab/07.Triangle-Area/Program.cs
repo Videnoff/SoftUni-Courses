c3456942487531c31f04856f2889ee1d8e5024ff
Videnoff <ivan.videnov@hotmail.com>
2021-04-13 20:37:04 +0000

    Add files via upload

---
+++ Programming Basics/02.Simple Operations and Calculations/01.Lab/07.Triangle-Area/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TriangleArea
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double side = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            double area = side * h / 2;
+            Console.WriteLine("{0:F2}", area);
+        }
+    }
+}
