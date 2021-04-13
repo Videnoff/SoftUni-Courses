9ee5fcab3f9dc267e8b25f68aa3191baffd63cab
Videnoff <ivan.videnov@hotmail.com>
2021-04-13 17:51:19 +0000

    Add files via upload

---
+++ JS Advanced/01.JS Advanced/06.JS-Advanced-Advanced-Functions/01.Lab/03.Currency-Format.js
@@ -0,0 +1,20 @@
+function currencyFormatter(separator, symbol, symbolFirst, value) {
+    let result = Math.trunc(value) + separator;
+    result += value.toFixed(2).substr(-2,2);
+    if (symbolFirst) return symbol + ' ' + result;
+    else return result + ' ' + symbol;
+}
+
+function createFormatter(separator, symbol, symbolFirst, currencyFormatter){
+    return format;
+
+    function format(input){
+        return currencyFormatter(separator, symbol, symbolFirst, input);
+    }
+}
+
+let dollarFormatter = createFormatter(',', '$', true, currencyFormatter);
+
+console.log(dollarFormatter(5345));
+console.log(dollarFormatter(3.1429));
+console.log(dollarFormatter(2.709));
