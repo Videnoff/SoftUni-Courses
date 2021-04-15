4f0f730e442a86de438ad5da8180afa329db5f05
Videnoff <ivan.videnov@hotmail.com>
2021-04-15 18:03:19 +0000

    Add files via upload

---
+++ Programming Basics/03.Conditional-Statements/01.Lab/04.Number-1-9-to-Text/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Number19toText
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine("one");
+                    break;
+                case 2:
+                    Console.WriteLine("two");
+                    break;
+                case 3:
+                    Console.WriteLine("three");
+                    break;
+                case 4:
+                    Console.WriteLine("four");
+                    break;
+                case 5:
+                    Console.WriteLine("five");
+                    break;
+                case 6:
+                    Console.WriteLine("six");
+                    break;
+                case 7:
+                    Console.WriteLine("seven");
+                    break;
+                case 8:
+                    Console.WriteLine("eight");
+                    break;
+                case 9:
+                    Console.WriteLine("nine");
+                    break;
+                default:
+                    Console.WriteLine("number too big");
+                    break;
+            }
+        }
+    }
+}
