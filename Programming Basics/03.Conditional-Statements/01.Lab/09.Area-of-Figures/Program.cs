d37e9752f41ec07ddc6c612ae578c2c66566603c
Videnoff <ivan.videnov@hotmail.com>
2021-04-15 18:05:42 +0000

    Add files via upload

---
+++ Programming Basics/03.Conditional-Statements/01.Lab/09.Area-of-Figures/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AreaofFigures
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+
+            if (figure == "square")
+            {
+                double side = double.Parse(Console.ReadLine());
+                double area = side * side;
+                Console.WriteLine($"{area:F3}");
+            }
+            else if (figure == "rectangle")
+            {
+                double side = double.Parse(Console.ReadLine());
+                double sideB = double.Parse(Console.ReadLine());
+                double area = side * sideB;
+                Console.WriteLine($"{area:F3}");
+            }
+            else if (figure == "circle")
+            {
+                double radius = double.Parse(Console.ReadLine());
+                double area = Math.PI * radius * radius;
+                Console.WriteLine($"{area:F3}");
+            }
+            else if (figure == "triangle")
+            {
+                double side = double.Parse(Console.ReadLine());
+                double h = double.Parse(Console.ReadLine());
+                double area = side * h / 2;
+                Console.WriteLine($"{area:F3}");
+            }
+        }
+    }
+}
