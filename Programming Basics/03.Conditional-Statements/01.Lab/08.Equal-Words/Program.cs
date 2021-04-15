ef45361d05b4a79d30c9ddf255ced8f272c557eb
Videnoff <ivan.videnov@hotmail.com>
2021-04-15 18:05:05 +0000

    Add files via upload

---
+++ Programming Basics/03.Conditional-Statements/01.Lab/08.Equal-Words/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EqualWords
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string firstWord = Console.ReadLine().ToLower();
+            string secondWord = Console.ReadLine().ToLower();
+
+            if (firstWord == secondWord)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
