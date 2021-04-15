073910effb91a5905f8229534fcf3118369ca88e
Videnoff <ivan.videnov@hotmail.com>
2021-04-15 18:07:02 +0000

    Add files via upload

---
+++ Programming Basics/03.Conditional-Statements/01.Lab/11.Animal-Type/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AnimalType
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string animal = Console.ReadLine();
+
+            switch (animal)
+            {
+                case "dog":
+                    Console.WriteLine("mammal");
+                    break;
+                case "crocodile":
+                    Console.WriteLine("reptile");
+                    break;
+                case "tortoise":
+                    Console.WriteLine("reptile");
+                    break;
+                case "snake":
+                    Console.WriteLine("reptile");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
