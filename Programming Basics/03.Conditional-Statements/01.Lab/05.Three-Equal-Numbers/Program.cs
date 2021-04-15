33175ea00ef13f950ab3730d06e45be5fb4bd5ab
Videnoff <ivan.videnov@hotmail.com>
2021-04-15 18:03:39 +0000

    Add files via upload

---
+++ Programming Basics/03.Conditional-Statements/01.Lab/05.Three-Equal-Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ThreeEqualNumbers
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            if (a == b && b == c && a == c)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
