2e2fdf745c71d2d6bca238e282841225c9188437
Videnoff <ivan.videnov@hotmail.com>
2021-04-15 18:02:58 +0000

    Add files via upload

---
+++ Programming Basics/03.Conditional-Statements/01.Lab/03.Even-or-Odd/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EvenorOdd
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}
