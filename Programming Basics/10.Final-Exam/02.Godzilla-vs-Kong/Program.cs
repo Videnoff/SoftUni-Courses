5df81eca8baf33a6f71d3a5070910a31dd302900
Videnoff <ivan.videnov@hotmail.com>
2021-04-16 19:27:17 +0000

    Add files via upload

---
+++ Programming Basics/10.Final-Exam/02.Godzilla-vs-Kong/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace GodzillavsKong
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            double statists = double.Parse(Console.ReadLine());
+            double statOutfitPrice = double.Parse(Console.ReadLine());
+
+            double decor = (budget * 10) / 100;
+            double statMoney = statists * statOutfitPrice;
+
+            double sum = decor + statMoney;
+            double discount = 0.0;
+            if (statists > 150)
+            {
+                discount = (statMoney * 10) / 100;
+                sum = decor + (statMoney - discount);
+            }
+            else
+            {
+                sum = decor + statMoney;
+            }
+            double diff = Math.Abs(budget - sum);
+            if (budget >= sum)
+            {
+                Console.WriteLine($"Action!");
+                Console.WriteLine($"Wingard starts filming with {diff:F2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money!");
+                Console.WriteLine($"Wingard needs {diff:F2} leva more.");
+            }
+        }
+    }
+}
