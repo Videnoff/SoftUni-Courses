053654792e535cfe7db3ca8a1ea8033ff2c143ad
Videnoff <ivan.videnov@hotmail.com>
2021-04-16 19:28:15 +0000

    Add files via upload

---
+++ Programming Basics/10.Final-Exam/03.Oscars-Week-in-Cinema/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace OscarsWeekinCinema
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string filmName = Console.ReadLine();
+            string hallType = Console.ReadLine();
+            int ticketsCount = int.Parse(Console.ReadLine());
+            double profit = 0.0;
+
+            if (filmName == "A Star Is Born")
+            {
+                if (hallType == "normal")
+                {
+                    profit = ticketsCount * 7.50;
+                }
+                else if (hallType == "luxury")
+                {
+                    profit = ticketsCount * 10.50;
+                }
+                else if (hallType == "ultra luxury")
+                {
+                    profit = ticketsCount * 13.50;
+                }
+            }
+            else if (filmName == "Bohemian Rhapsody")
+            {
+                if (hallType == "normal")
+                {
+                    profit = ticketsCount * 7.35;
+                }
+                else if (hallType == "luxury")
+                {
+                    profit = ticketsCount * 9.45;
+                }
+                else if (hallType == "ultra luxury")
+                {
+                    profit = ticketsCount * 12.75;
+                }
+            }
+            else if (filmName == "Green Book")
+            {
+                if (hallType == "normal")
+                {
+                    profit = ticketsCount * 8.15;
+                }
+                else if (hallType == "luxury")
+                {
+                    profit = ticketsCount * 10.25;
+                }
+                else if (hallType == "ultra luxury")
+                {
+                    profit = ticketsCount * 13.25;
+                }
+            }
+            else if (filmName == "The Favourite")
+            {
+                if (hallType == "normal")
+                {
+                    profit = ticketsCount * 8.75;
+                }
+                else if (hallType == "luxury")
+                {
+                    profit = ticketsCount * 11.55;
+                }
+                else if (hallType == "ultra luxury")
+                {
+                    profit = ticketsCount * 13.95;
+                }
+            }
+
+            Console.WriteLine($"{filmName} -> {profit:F2} lv.");
+        }
+    }
+}
