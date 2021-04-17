d810a1d35e396a030bb2a46cd55b94f7a2e42dff
Videnoff <ivan.videnov@hotmail.com>
2021-04-17 18:16:20 +0000

    Add files via upload

---
+++ Programming Basics/09.PB-Sample Exam-31-March-2019/06.Multiply-Table/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MultiplyTable
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int singleDigitNum = 0;
+            int doubleDigitNum = 0;
+            int tripleDigitNum = 0;
+
+            singleDigitNum = n % 10;
+            doubleDigitNum = (n / 10) % 10;
+            tripleDigitNum = n / 100;
+
+            for (int i = 1; i <= singleDigitNum; i++)
+            {
+                for (int j = 1; j <= doubleDigitNum; j++)
+                {
+                    for (int k = 1; k <= tripleDigitNum; k++)
+                    {
+                        Console.WriteLine($"{i} * {j} * {k} = {i * j * k};");
+                    }
+                }
+            }
+        }
+    }
+}
