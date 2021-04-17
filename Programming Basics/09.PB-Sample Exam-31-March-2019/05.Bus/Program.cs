47d60ac18d3faa48f8aa6a794c2c6b2f34518bb7
Videnoff <ivan.videnov@hotmail.com>
2021-04-17 18:13:29 +0000

    Add files via upload

---
+++ Programming Basics/09.PB-Sample Exam-31-March-2019/05.Bus/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Bus
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int startPassengers = int.Parse(Console.ReadLine());
+            int busStop = int.Parse(Console.ReadLine());
+            int conductors = 2;
+            int stopsCount = 1;
+            for (int i = 0; i < busStop; i++)
+            {
+                int downPass = int.Parse(Console.ReadLine());
+                int upPass = int.Parse(Console.ReadLine());
+
+                if (stopsCount % 2 == 0)
+                {
+                    stopsCount++;
+                    startPassengers -= downPass;
+                    startPassengers += upPass;
+                    startPassengers -= conductors;
+                }
+                else
+                {
+                    stopsCount++;
+                    startPassengers -= downPass;
+                    startPassengers += upPass;
+                    startPassengers += conductors;
+                }
+            }
+            Console.WriteLine($"The final number of passengers is : {startPassengers}");
+        }
+    }
+}
