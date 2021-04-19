946c317c12fbd8343993abef27440e763e059235
Videnoff <ivan.videnov@hotmail.com>
2021-04-19 15:31:02 +0000

    Add files via upload

---
+++ Programming Basics/07.Nested-Loops/01.Lab/05.Building/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Building
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int floorCount = int.Parse(Console.ReadLine());
+            int rooms = int.Parse(Console.ReadLine());
+
+            for (int i = floorCount; i >= 1; i--)
+            {
+                for (int j = 0; j < rooms; j++)
+                {
+                    if (i == floorCount)
+                    {
+                        Console.Write($"L{i}{j} ");
+                    }
+                    else if (i % 2 == 0)
+                    {
+                        Console.Write($"O{i}{j} ");
+                    }
+                    else
+                    {
+                        Console.Write($"A{i}{j} ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
