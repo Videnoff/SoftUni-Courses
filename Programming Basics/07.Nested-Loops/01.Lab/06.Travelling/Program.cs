0ce02d1ac29f604f8d2c49d3a9b4b908edf718b7
Videnoff <ivan.videnov@hotmail.com>
2021-04-19 15:31:34 +0000

    Add files via upload

---
+++ Programming Basics/07.Nested-Loops/01.Lab/06.Travelling/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Travelling
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+
+            while (destination != "End")
+            {
+                double neededMoney = double.Parse(Console.ReadLine());
+                double savedMoney = 0.0;
+                while (savedMoney < neededMoney)
+                {
+                    double currentMoney = double.Parse(Console.ReadLine());
+                    savedMoney += currentMoney;
+                }
+                Console.WriteLine($"Going to {destination}!");
+                destination = Console.ReadLine();
+            }
+        }
+    }
+}
