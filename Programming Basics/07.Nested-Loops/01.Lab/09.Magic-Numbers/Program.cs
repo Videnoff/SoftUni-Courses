63431e0bf70abb4b4f6f28d5fc4267d304adc056
Videnoff <ivan.videnov@hotmail.com>
2021-04-19 15:33:10 +0000

    Add files via upload

---
+++ Programming Basics/07.Nested-Loops/01.Lab/09.Magic-Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MagicNumbers
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    for (int k = 1; k <= 9; k++)
+                    {
+                        for (int x = 1; x <= 9; x++)
+                        {
+                            for (int y = 1; y <= 9; y++)
+                            {
+                                for (int z = 1; z <= 9; z++)
+                                {
+                                    int num = i * j * k * x * y * z;
+                                    if (num == n)
+                                    {
+                                        Console.Write($"{i}{j}{k}{x}{y}{z} ");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
