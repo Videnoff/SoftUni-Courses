e1b853807c8577fa412e2dd2c035e6473ac6bc67
Videnoff <ivan.videnov@hotmail.com>
2021-04-19 15:32:03 +0000

    Add files via upload

---
+++ Programming Basics/07.Nested-Loops/01.Lab/07.Name-Wars/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NameWars
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int maxSum = int.MinValue;
+            string winner = string.Empty;
+
+            while (name != "STOP")
+            {
+                int sum = 0;
+                for (int i = 0; i < name.Length; i++)
+                {
+                    char curr = name[i];
+                    sum += (int)curr;
+                }
+
+                if (sum > maxSum)
+                {
+                    maxSum = sum;
+                    winner = name;
+                }
+                name = Console.ReadLine();
+            }
+            Console.WriteLine($"Winner is {winner} - {maxSum}!");
+        }
+    }
+}
