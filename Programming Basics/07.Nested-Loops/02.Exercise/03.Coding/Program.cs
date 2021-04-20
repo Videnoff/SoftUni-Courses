8de0a192f5e57bfd4ca54a68fcc2fc4565b9e2df
Videnoff <ivan.videnov@hotmail.com>
2021-04-20 17:49:22 +0000

    Add files via upload

---
+++ Programming Basics/07.Nested-Loops/02.Exercise/03.Coding/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Coding
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                char currentDig = input[i];
+                int currentDigitToNum = int.Parse(currentDig.ToString());
+                //Console.WriteLine(currentDigitToNum);
+
+                if (currentDigitToNum == 0)
+                {
+                    Console.WriteLine("ZERO");
+                    continue;
+                }
+                for (int j = 1; j <= currentDigitToNum; j++)
+                {
+                    Console.Write((char)(currentDigitToNum + 33));
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
