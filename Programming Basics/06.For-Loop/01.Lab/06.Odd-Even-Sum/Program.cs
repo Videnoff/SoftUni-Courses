632c82aa6f4ba195cbd01480834703dd55929ccf
Videnoff <ivan.videnov@hotmail.com>
2021-04-21 08:27:50 +0000

    Add files via upload

---
+++ Programming Basics/06.For-Loop/01.Lab/06.Odd-Even-Sum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OddEvenSum
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sumEven = 0;
+            int sumOdd = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    sumEven += a;
+                }
+                else
+                {
+                    sumOdd += a;
+                }
+            }
+            int diff = Math.Abs(sumOdd - sumEven);
+            if (sumOdd == sumEven)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {sumOdd}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {diff}");
+            }
+        }
+    }
+}
