8bad37d294e4811e73c11474f1a65af3f0685fcb
Videnoff <ivan.videnov@hotmail.com>
2021-04-21 08:29:19 +0000

    Add files via upload

---
+++ Programming Basics/06.For-Loop/01.Lab/08.Clever-Lily/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CleverLily
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int lilisAge = int.Parse(Console.ReadLine());
+            double washmashinePrice = double.Parse(Console.ReadLine());
+            double toyPrice = int.Parse(Console.ReadLine());
+            double toys = 0;
+            double money = 0;
+            double brother = 1.00;
+
+            for (int i = 1; i <= lilisAge; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    money += (i * 10 / 2);
+                    money -= brother;
+                }
+                else
+                {
+                    toys++;
+                }
+            }
+            double toysSum = toys * toyPrice;
+            double totalMoney = money + toysSum;
+            double diff = Math.Abs(totalMoney - washmashinePrice);
+
+            if (totalMoney >= washmashinePrice)
+            {
+                Console.WriteLine($"Yes! {diff:F2}");
+            }
+            else
+            {
+                Console.WriteLine($"No! {diff:F2}");
+            }
+        }
+    }
+}
