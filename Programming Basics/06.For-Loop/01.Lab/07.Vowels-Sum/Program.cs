f175e8490a54989abf16421cd77c32f6c8b0e7b5
Videnoff <ivan.videnov@hotmail.com>
2021-04-21 08:28:23 +0000

    Add files via upload

---
+++ Programming Basics/06.For-Loop/01.Lab/07.Vowels-Sum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace VowelsSum
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int sum = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char letter = s[i];
+                if (letter == 'a')
+                {
+                    sum += 1;
+                }
+                else if (letter == 'e')
+                {
+                    sum += 2;
+                }
+                else if (letter == 'i')
+                {
+                    sum += 3;
+                }
+                else if (letter == 'o')
+                {
+                    sum += 4;
+                }
+                else if (letter == 'u')
+                {
+                    sum += 5;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
