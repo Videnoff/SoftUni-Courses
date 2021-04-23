c9347fa71216626193596d4242afd910c1cfaffc
Videnoff <ivan.videnov@hotmail.com>
2021-04-23 09:27:32 +0000

    Add files via upload

---
+++ Programming Basics/06.For-Loop/02.Exercise/06.Divide-Without-Remainder/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DivideWithoutRemainder
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double p1 = 0.0;
+            double p2 = 0.0;
+            double p3 = 0.0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                double num = double.Parse(Console.ReadLine());
+
+                if (num % 2 == 0)
+                {
+                    p1++;
+                }
+                if (num % 3 == 0)
+                {
+                    p2++;
+                }
+                if (num % 4 == 0)
+                {
+                    p3++;
+                }
+            }
+
+            Console.WriteLine($"{(p1 / n) * 100:F2}%");
+            Console.WriteLine($"{(p2 / n) * 100:F2}%");
+            Console.WriteLine($"{(p3 / n) * 100:F2}%");
+        }
+    }
+}
