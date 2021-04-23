879bfbae29197166b7935d2efb2a225396a5ec85
Videnoff <ivan.videnov@hotmail.com>
2021-04-23 09:33:55 +0000

    Add files via upload

---
+++ Programming Basics/04.While-Loop/01.Lab/08.Moving/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Moving
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int lenght = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            string packets = Console.ReadLine();
+            int homeVolume = width * lenght * height;
+            int totalBoxes = 0;
+
+            while (packets != "Done")
+            {
+                int boxes = int.Parse(packets);
+                totalBoxes += boxes;
+                if (totalBoxes >= homeVolume)
+                {
+                    Console.WriteLine($"No more free space! You need {totalBoxes - homeVolume} Cubic meters more.");
+                    break;
+                }
+                packets = Console.ReadLine();
+            }
+            if (packets == "Done")
+            {
+                Console.WriteLine($"{homeVolume - totalBoxes} Cubic meters left.");
+            }
+        }
+    }
+}
