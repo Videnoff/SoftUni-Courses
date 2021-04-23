42edcad5323eb17e3d3cac4b17693ba2f82c2c07
Videnoff <ivan.videnov@hotmail.com>
2021-04-23 09:37:28 +0000

    Add files via upload

---
+++ Programming Basics/04.While-Loop/02.Exercise/06.Cake/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Cake
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int pieces = a * b;
+            string command = Console.ReadLine();
+            int numberTakenPieces = 0;
+            while (command != "STOP" && numberTakenPieces <= pieces)
+            {
+                int takePieces = int.Parse(command);
+                numberTakenPieces += takePieces;
+                command = Console.ReadLine();
+            }
+            int difference = pieces - numberTakenPieces;
+            if (command == "STOP" && difference > 0)
+            {
+                Console.WriteLine($"{difference} pieces are left.");
+            }
+            else if (difference < 0)
+            {
+                difference = Math.Abs(difference);
+                Console.WriteLine($"No more cake left! You need {difference} pieces more.");
+            }
+        }
+    }
+}
