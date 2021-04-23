3210559d1c3d926cd82a0eef634b29adf18d20bb
Videnoff <ivan.videnov@hotmail.com>
2021-04-23 09:28:42 +0000

    Add files via upload

---
+++ Programming Basics/06.For-Loop/02.Exercise/07.Salary/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Salary
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                string site = Console.ReadLine();
+                if (site == "Facebook")
+                {
+                    salary -= 150;
+                }
+                if (site == "Instagram")
+                {
+                    salary -= 100;
+                }
+                if (site == "Reddit")
+                {
+                    salary -= 50;
+                }
+            }
+            if (salary > 0)
+            {
+                Console.WriteLine($"{salary}");
+            }
+            else
+            {
+                Console.WriteLine("You have lost your salary.");
+            }
+        }
+    }
+}
