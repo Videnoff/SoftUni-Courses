adfc82b1b0986488ae83748f47bc90996dcb2d84
Videnoff <ivan.videnov@hotmail.com>
2021-04-23 09:34:59 +0000

    Add files via upload

---
+++ Programming Basics/04.While-Loop/02.Exercise/02.Exam-Preparation/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ExamPreparation
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int poorGrades = int.Parse(Console.ReadLine());
+            int countPoorGrades = 0;
+            double sumGrades = 0;
+            int counterForGrades = 0;
+            string lastProblem = "";
+
+            while (countPoorGrades < poorGrades)
+            {
+                string problemName = Console.ReadLine();
+                if (problemName != "Enough")
+                {
+                    lastProblem = problemName;
+                }
+                if (problemName == "Enough")
+                {
+                    double averageGrade = sumGrades / counterForGrades;
+                    Console.WriteLine($"Average score: {averageGrade:F2}");
+                    Console.WriteLine($"Number of problems: {counterForGrades}");
+                    Console.WriteLine($"Last problem: {lastProblem}");
+                    break;
+                }
+                int grade = int.Parse(Console.ReadLine());
+
+                sumGrades += grade;
+                counterForGrades++;
+
+                if (grade <= 4)
+                {
+                    countPoorGrades ++;;
+                }
+
+            }
+
+            if (countPoorGrades == poorGrades)
+            {
+                Console.WriteLine($"You need a break, {countPoorGrades} poor grades.");
+            }
+        }
+    }
+}
