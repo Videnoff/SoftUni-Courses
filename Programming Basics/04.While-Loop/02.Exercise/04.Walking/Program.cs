b6bb5802833aee39c080dbe0d1b9cd5e00beb082
Videnoff <ivan.videnov@hotmail.com>
2021-04-23 09:36:19 +0000

    Add files via upload

---
+++ Programming Basics/04.While-Loop/02.Exercise/04.Walking/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Walking
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int steps = 0;
+            int diff = 0;
+            while (steps < 10000)
+            {
+                string s = Console.ReadLine();
+
+                if (s == "Going home")
+                {
+                    steps += int.Parse(Console.ReadLine());
+                    if (steps >= 10000)
+                    {
+                        Console.WriteLine("Goal reached! Good job!");
+                    }
+                    else
+                    {
+
+                        diff = 10000 - steps;
+                        Console.WriteLine($"{diff} more steps to reach goal.");
+                    }
+                    break;
+                }
+                else
+                {
+                    steps += int.Parse(s);
+                    if (steps >= 10000)
+                    {
+                        Console.WriteLine("Goal reached! Good job!");
+                    }
+                }
+            }
+        }
+    }
+}
