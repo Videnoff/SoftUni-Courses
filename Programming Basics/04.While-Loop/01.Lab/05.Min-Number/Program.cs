0d9d45d48b173219946aaaed474ee996ee82e94a
Videnoff <ivan.videnov@hotmail.com>
2021-04-23 09:32:16 +0000

    Add files via upload

---
+++ Programming Basics/04.While-Loop/01.Lab/05.Min-Number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MinNumber
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int counter = 0;
+            int max = int.MaxValue;
+            while (counter < n)
+            {
+                int a = int.Parse(Console.ReadLine());
+                counter++;
+
+                if (a < max)
+                {
+                    max = a;
+                }
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
