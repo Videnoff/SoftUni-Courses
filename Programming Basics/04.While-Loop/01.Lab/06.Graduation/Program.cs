c9956ae82a65d67d4cff415ff92bf65656d2109b
Videnoff <ivan.videnov@hotmail.com>
2021-04-23 09:32:50 +0000

    Add files via upload

---
+++ Programming Basics/04.While-Loop/01.Lab/06.Graduation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Graduation
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int counter = 1;
+            double sum = 0;
+
+
+            while (counter <= 12)
+            {
+                double grade = double.Parse(Console.ReadLine());
+                if (grade >= 4.00)
+                {
+                    sum += grade;
+                    counter++;
+                }
+            }
+            double average = sum / 12;
+            Console.WriteLine($"{s} graduated. Average grade: {average:F2}");
+        }
+    }
+}
