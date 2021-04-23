a54948780bbbacaf67566785fcc819126d4208f3
Videnoff <ivan.videnov@hotmail.com>
2021-04-23 09:31:41 +0000

    Add files via upload

---
+++ Programming Basics/04.While-Loop/01.Lab/04.Max-Number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MaxNumber
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int maxNumber = int.MinValue;
+            int minNumber = int.MaxValue;
+
+            while (s != "END")
+            {
+                int n = int.Parse(s);
+
+                if (n < minNumber)
+                {
+                    minNumber = n;
+                }
+                if (n > maxNumber)
+                {
+                    maxNumber = n;
+                }
+                s = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Max number: {maxNumber}");
+            Console.WriteLine($"Min number: {minNumber}");
+        }
+    }
+}
