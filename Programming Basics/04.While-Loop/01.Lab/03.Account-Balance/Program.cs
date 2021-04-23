bf76c553fcbf9f54f2b141ea9388f69f992f162b
Videnoff <ivan.videnov@hotmail.com>
2021-04-23 09:31:03 +0000

    Add files via upload

---
+++ Programming Basics/04.While-Loop/01.Lab/03.Account-Balance/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AccountBalance
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int counter = 0;
+            double balance = 0.0;
+            while (counter < n)
+            {
+                double k = double.Parse(Console.ReadLine());
+                if (k < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                Console.WriteLine($"Increase: {k:F2}");
+                counter++;
+                balance += k;
+            }
+            Console.WriteLine($"Total: {balance:F2}");
+        }
+    }
+}
