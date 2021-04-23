522e7e8bb477c7f6caf8f4f05316b97c46528fb9
Videnoff <ivan.videnov@hotmail.com>
2021-04-23 09:24:51 +0000

    Add files via upload

---
+++ Programming Basics/06.For-Loop/02.Exercise/02.Half-Sum-Element/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HalfSumElement
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int maxNum = int.MinValue;
+            int minNum = int.MaxValue;
+            for (int i = 1; i <= n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                sum += num;
+                if (num > maxNum)
+                {
+                    maxNum = num;
+                }
+                if (num < minNum)
+                {
+                    minNum = num;
+                }
+            }
+            int diff = Math.Abs(maxNum - (sum - maxNum));
+            if (maxNum == (sum - maxNum))
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {maxNum}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {diff}");
+            }
+        }
+    }
+}
