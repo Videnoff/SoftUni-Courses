2e45dac5cf167de5910ad36b0247e757b4884379
Videnoff <ivan.videnov@hotmail.com>
2021-04-23 09:29:54 +0000

    Add files via upload

---
+++ Programming Basics/04.While-Loop/01.Lab/02.Sequence-2k-plus-1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Sequence2kplus1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = 1;
+
+            while (k <= n)
+            {
+                Console.WriteLine(k);
+                k = k * 2 + 1;
+            }
+        }
+    }
+}
