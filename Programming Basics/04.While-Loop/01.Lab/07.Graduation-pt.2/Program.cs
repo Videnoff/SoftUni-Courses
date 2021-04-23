cbc3a7768569fa7273388b67d968d5e30784ef64
Videnoff <ivan.videnov@hotmail.com>
2021-04-23 09:33:23 +0000

    Add files via upload

---
+++ Programming Basics/04.While-Loop/01.Lab/07.Graduation-pt.2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Graduationpt
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int counter = 1;
+            double sum = 0;
+            double excluded = 0;
+            bool isExcluded = false;
+
+
+            while (counter <= 12)
+            {
+                double grade = double.Parse(Console.ReadLine());
+                if (grade < 4.00)
+                {
+                    excluded++;
+                }
+                else if (grade >= 4.00)
+                {
+                    sum += grade;
+                    counter++;
+                }
+                if (excluded >= 2)
+                {
+                    isExcluded = true;
+                    break;
+                }
+            }
+
+            if (isExcluded == false)
+            {
+                double average = sum / 12;
+                Console.WriteLine($"{s} graduated. Average grade: {average:F2}");
+            }
+            else
+            {
+                Console.WriteLine($"{s} has been excluded at {counter} grade");
+            }
+        }
+    }
+}
