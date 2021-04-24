1130f95db45d99af3437b70e3edce2fe07f6bd29
Videnoff <ivan.videnov@hotmail.com>
2021-04-24 09:59:45 +0000

    Add files via upload

---
+++ Programming Basics/03.Nested-Conditional-Statements/01.Lab/05.Invalid-Number/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace InvalidNumber
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            if (!(n == 0 || n >= 100 && n <= 200))
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}
