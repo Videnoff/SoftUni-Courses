c3cce08358fa1310ab866340e6965cca158b8a48
Videnoff <ivan.videnov@hotmail.com>
2021-04-26 08:16:41 +0000

    Add files via upload

---
+++ Programming Basics/03.Nested-Conditional-Statements/02.Exercise/06.Journey/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Journey
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            double moneySpent = 0;
+            string destination = "";
+            string holidayType = "";
+
+            if (budget <= 100)
+            {
+                destination = "Bulgaria";
+                if (season == "summer")
+                {
+                    holidayType = "Camp";
+                    moneySpent = budget * 30 / 100;
+                }
+                else if (season == "winter")
+                {
+                    holidayType = "Hotel";
+                    moneySpent = budget * 70 / 100;
+                }
+            }
+            else if (budget <= 1000)
+            {
+                destination = "Balkans";
+                if (season == "summer")
+                {
+                    holidayType = "Camp";
+                    moneySpent = budget * 40 / 100;
+                }
+                else if (season == "winter")
+                {
+                    holidayType = "Hotel";
+                    moneySpent = budget * 80 / 100;
+                }
+            }
+            else if (budget > 1000)
+            {
+                destination = "Europe";
+                holidayType = "Hotel";
+                moneySpent = budget * 90 / 100;
+            }
+
+            Console.WriteLine($"Somewhere in {destination}");
+            Console.WriteLine($"{holidayType} - {moneySpent:F2}");
+        }
+    }
+}
