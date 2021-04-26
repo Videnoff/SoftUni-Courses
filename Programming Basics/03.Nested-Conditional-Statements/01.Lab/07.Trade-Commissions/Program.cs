b082ee711af87828750e345fa9040be7b48cb191
Videnoff <ivan.videnov@hotmail.com>
2021-04-26 08:13:07 +0000

    Add files via upload

---
+++ Programming Basics/03.Nested-Conditional-Statements/01.Lab/07.Trade-Commissions/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace TradeCommissions
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string town = Console.ReadLine();
+            double tradeVolume = double.Parse(Console.ReadLine());
+            double comission = 0;
+
+            if (town == "Sofia")
+            {
+                if (tradeVolume >= 0 && tradeVolume <= 500)
+                {
+                    comission = (tradeVolume * 5) / 100;
+                    Console.WriteLine($"{comission:F2}");
+                }
+                else if (tradeVolume > 500 && tradeVolume <= 1000)
+                {
+                    comission = (tradeVolume * 7) / 100;
+                    Console.WriteLine($"{comission:F2}");
+                }
+                else if (tradeVolume > 1000 && tradeVolume <= 10000)
+                {
+                    comission = (tradeVolume * 8) / 100;
+                    Console.WriteLine($"{comission:F2}");
+                }
+                else if (tradeVolume > 10000)
+                {
+                    comission = (tradeVolume * 12) / 100;
+                    Console.WriteLine($"{comission:F2}");
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+            }
+            else if (town == "Varna")
+            {
+                if (tradeVolume >= 0 && tradeVolume <= 500)
+                {
+                    comission = (tradeVolume * 4.5) / 100;
+                    Console.WriteLine($"{comission:F2}");
+                }
+                else if (tradeVolume > 500 && tradeVolume <= 1000)
+                {
+                    comission = (tradeVolume * 7.5) / 100;
+                    Console.WriteLine($"{comission:F2}");
+                }
+                else if (tradeVolume > 1000 && tradeVolume <= 10000)
+                {
+                    comission = (tradeVolume * 10) / 100;
+                    Console.WriteLine($"{comission:F2}");
+                }
+                else if (tradeVolume > 10000)
+                {
+                    comission = (tradeVolume * 13) / 100;
+                    Console.WriteLine($"{comission:F2}");
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+            }
+            else if (town == "Plovdiv")
+            {
+                if (tradeVolume >= 0 && tradeVolume <= 500)
+                {
+                    comission = (tradeVolume * 5.5) / 100;
+                    Console.WriteLine($"{comission:F2}");
+                }
+                else if (tradeVolume > 500 && tradeVolume <= 1000)
+                {
+                    comission = (tradeVolume * 8) / 100;
+                    Console.WriteLine($"{comission:F2}");
+                }
+                else if (tradeVolume > 1000 && tradeVolume <= 10000)
+                {
+                    comission = (tradeVolume * 12) / 100;
+                    Console.WriteLine($"{comission:F2}");
+                }
+                else if (tradeVolume > 10000)
+                {
+                    comission = (tradeVolume * 14.5) / 100;
+                    Console.WriteLine($"{comission:F2}");
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+        }
+    }
+}
