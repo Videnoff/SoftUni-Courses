54762dadb3aa10d41a50ca85850f0e9962a5f8ea
Videnoff <ivan.videnov@hotmail.com>
2021-04-26 08:14:32 +0000

    Add files via upload

---
+++ Programming Basics/03.Nested-Conditional-Statements/02.Exercise/02.Cinema/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Cinema
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string projection = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+            double sum = rows * cols;
+
+            if (projection == "Premiere")
+            {
+                sum *= 12.00;
+                Console.WriteLine($"{sum:F2} leva");
+            }
+            else if (projection == "Normal")
+            {
+                sum *= 7.50;
+                Console.WriteLine($"{sum:F2} leva");
+            }
+            else if (projection == "Discount")
+            {
+                sum *= 5.00;
+                Console.WriteLine($"{sum:F2} leva");
+            }
+        }
+    }
+}
