22dde185b093e95c14a8590a08cb17d1ff0cfdd6
Videnoff <ivan.videnov@hotmail.com>
2021-04-26 16:10:59 +0000

    Add files via upload

---
+++ Programming Basics/03.Nested-Conditional-Statements/02.Exercise/07.Operations-Between-Numbers/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace OperationsBetweenNumbers
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double N1 = int.Parse(Console.ReadLine());
+            double N2 = int.Parse(Console.ReadLine());
+            char ch = char.Parse(Console.ReadLine());
+            double result = 0.0;
+            string oddEven = "";
+
+            if (ch == '+')
+            {
+                result = N1 + N2;
+                if (result % 2 != 0)
+                {
+                    oddEven = "odd";
+                    Console.WriteLine($"{N1} {ch} {N2} = {result} - {oddEven}");
+                }
+                else
+                {
+                    oddEven = "even";
+                    Console.WriteLine($"{N1} {ch} {N2} = {result} - {oddEven}");
+                }
+            }
+            else if (ch == '-')
+            {
+                result = N1 - N2;
+                if (result % 2 != 0)
+                {
+                    oddEven = "odd";
+                    Console.WriteLine($"{N1} {ch} {N2} = {result} - {oddEven}");
+                }
+                else
+                {
+                    oddEven = "even";
+                    Console.WriteLine($"{N1} {ch} {N2} = {result} - {oddEven}");
+                }
+            }
+            else if (ch == '*')
+            {
+                result = N1 * N2;
+                if (result % 2 != 0)
+                {
+                    oddEven = "odd";
+                    Console.WriteLine($"{N1} {ch} {N2} = {result} - {oddEven}");
+                }
+                else
+                {
+                    oddEven = "even";
+                    Console.WriteLine($"{N1} {ch} {N2} = {result} - {oddEven}");
+                }
+            }
+            else if (ch == '/')
+            {
+                result = N1 / N2;
+                if (N2 == 0)
+                {
+                    Console.WriteLine($"Cannot divide {N1} by zero");
+                }
+                else
+                {
+                    Console.WriteLine($"{N1} / {N2} = {result:F2}");
+                }
+            }
+            else if (ch == '%')
+            {
+                result = N1 % N2;
+                if (N2 == 0)
+                {
+                    Console.WriteLine($"Cannot divide {N1} by zero");
+                }
+                else
+                {
+                    Console.WriteLine($"{N1} % {N2} = {result}");
+                }
+            }
+        }
+    }
+}
