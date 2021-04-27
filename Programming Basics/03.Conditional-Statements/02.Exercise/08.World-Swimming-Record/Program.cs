868760f6df3f7cb34ff65420bdc6e51e8594ade5
Videnoff <ivan.videnov@hotmail.com>
2021-04-27 15:37:10 +0000

    Add files via upload

---
+++ Programming Basics/03.Conditional-Statements/02.Exercise/08.World-Swimming-Record/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace WorldSwimmingRecord
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double recordTime = double.Parse(Console.ReadLine());
+            double recordDistance = double.Parse(Console.ReadLine());
+            double timeFor1meter = double.Parse(Console.ReadLine());
+
+            double ivanDistance = recordDistance * timeFor1meter;
+            double waterResistance = Math.Floor(recordDistance / 15) * 12.5;
+
+            double ivanTime = ivanDistance + waterResistance;
+            double diff = Math.Abs(recordTime - ivanTime);
+
+            if (ivanTime < recordTime)
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {ivanTime:F2} seconds.");
+            }
+            else
+            {
+                Console.WriteLine($"No, he failed! He was {diff:F2} seconds slower.");
+            }
+        }
+    }
+}
