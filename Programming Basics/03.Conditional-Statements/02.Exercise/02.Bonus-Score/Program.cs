4585de72a34a417be0a83945590079c693fc109e
Videnoff <ivan.videnov@hotmail.com>
2021-04-27 15:33:42 +0000

    Add files via upload

---
+++ Programming Basics/03.Conditional-Statements/02.Exercise/02.Bonus-Score/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace BonusScore
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double bonus = 0;
+            if (n <= 100)
+            {
+                bonus += 5;
+                if (n % 2 == 0)
+                {
+                    bonus++;
+                }
+                if (n % 10 == 5)
+                {
+                    bonus += 2;
+                }
+               
+            }
+            else if (n > 100 && n <= 1000)
+            {
+                if (n % 2 == 0)
+                {
+                    bonus++;
+                }
+                if (n % 10 == 5)
+                {
+                    bonus += 2;
+                }
+                bonus += n * 0.2;
+            }
+            else if (n > 1000)
+            {
+                if (n % 2 == 0)
+                {
+                    bonus++;
+                }
+                if (n % 10 == 5)
+                {
+                    bonus += 2;
+                }
+                bonus += n * 0.1;
+            }
+            Console.WriteLine(bonus);
+            Console.WriteLine(n + bonus);
+        }
+    }
+}
