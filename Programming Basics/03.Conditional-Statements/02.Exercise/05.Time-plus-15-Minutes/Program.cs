fbfd29f4554d284df36daf76ac055457191d84f9
Videnoff <ivan.videnov@hotmail.com>
2021-04-27 15:35:32 +0000

    Add files via upload

---
+++ Programming Basics/03.Conditional-Statements/02.Exercise/05.Time-plus-15-Minutes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Timeplus15Minutes
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            int startSum = (hour * 60) + minutes;
+            int endSum = startSum + 15;
+            int timePlus15 = endSum / 60;
+            int minsPlus15 = endSum % 60;
+
+            if (timePlus15 >= 24)
+            {
+                timePlus15 -= 24;   
+            }
+
+            Console.WriteLine($"{timePlus15}:{minsPlus15:D2}");
+        }
+    }
+}
