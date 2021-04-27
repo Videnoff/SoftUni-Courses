747ad747b1baa32baf3a5ec86651018ab7c4e188
Videnoff <ivan.videnov@hotmail.com>
2021-04-27 15:34:53 +0000

    Add files via upload

---
+++ Programming Basics/03.Conditional-Statements/02.Exercise/04.Metric-Converter/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace MetricConverter
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+            string unitIn = Console.ReadLine();
+            string unitOut = Console.ReadLine();
+
+            if (unitIn == "mm")
+            {
+                if (unitOut == "mm")
+                {
+                    n += n;
+                }
+                else if (unitOut == "cm")
+                {
+                    n /= 10;
+                }
+                else if (unitOut == "m")
+                {
+                    n /= 1000; 
+                }
+            }
+            else if (unitIn == "cm")
+            {
+                if (unitOut == "cm")
+                {
+                    n += n;
+                }
+                else if (unitOut == "mm")
+                {
+                    n *= 10;
+                }
+                else if (unitOut == "m")
+                {
+                    n /= 100;
+                }
+            }
+            else if (unitIn == "m")
+            {
+                if (unitOut == "m")
+                {
+                    n += n;
+                }
+                else if (unitOut == "mm")
+                {
+                    n *= 1000;
+                }
+                else if (unitOut == "cm")
+                {
+                    n *= 100;;
+                }
+            }
+            Console.WriteLine($"{n:F3}");
+        }
+    }
+}
