b429058b1d42af68a6be5fd95c389948b4c9a404
Videnoff <ivan.videnov@hotmail.com>
2021-04-28 16:37:22 +0000

    Add files via upload

---
+++ JS Advanced/01.JS Advanced/05.JS-Advanced-DOM-Events/01.Lab/06. Dynamic-Validation/app.js
@@ -0,0 +1,14 @@
+function validate() {
+
+    document.querySelector('#email').addEventListener('change', onChange);
+
+    function onChange(ev) {
+        const email = ev.target.value;
+
+        if (/^[a-z]+@[a-z]+\.[a-z]+$/.test(email)) {
+            ev.target.className = '';
+        } else {
+            ev.target.className = 'error';
+        }
+    }
+}
