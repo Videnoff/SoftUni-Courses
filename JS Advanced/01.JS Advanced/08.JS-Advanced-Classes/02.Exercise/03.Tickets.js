ef8c686bcc7b01db95bb26bf666c219007ebbfc9
Videnoff <ivan.videnov@hotmail.com>
2021-05-12 22:53:32 +0300

    Create 03.Tickets.js

---
+++ JS Advanced/01.JS Advanced/08.JS-Advanced-Classes/02.Exercise/03.Tickets.js
@@ -0,0 +1,45 @@
+// "use strict";
+
+function solve(arr, criteria) {
+    function splitLine(line) {
+        return line.split('|');
+    }
+
+    function convertToTicket([destination, price, status]) {
+        return new Ticket(destination, Number(price), status)
+    }
+
+    const sortMapper = {
+        'destination': (a, b) => a.destination.localeCompare(b.destination),
+        'price': (a, b) => a.price - b.price,
+        'status': (a, b) => a.status.localeCompare(b.status)
+    }
+
+    class Ticket {
+        constructor(destination, price, status) {
+            this.destination = destination;
+            this.price = price;
+            this.status = status;
+        }
+
+    }
+
+    return arr
+        .map(splitLine)
+        .map(convertToTicket)
+        .sort(sortMapper[criteria]);
+}
+
+console.log(solve(['Philadelphia|94.20|available',
+        'New York City|95.99|available',
+        'New York City|95.99|sold',
+        'Boston|126.20|departed'],
+    'destination'
+));
+
+console.log(solve(['Philadelphia|94.20|available',
+        'New York City|95.99|available',
+        'New York City|95.99|sold',
+        'Boston|126.20|departed'],
+    'status'
+));
