f1ed52f2de4c2a695ff31e8c03c914488f1008cd
Videnoff <ivan.videnov@hotmail.com>
2021-05-15 20:11:59 +0000

    Add files via upload

---
+++ JS Advanced/01.JS Advanced/09.JS-Advanced-Prototypes-and-Inheritance/01.Lab/03.Inheriting-and-Replacing-ToString.js
@@ -0,0 +1,47 @@
+function toStringExtension() {
+    class Person {
+        constructor(name, email) {
+            this.name = name;
+            this.email = email;
+        }
+        toString(){
+            return `${this.constructor.name} (name: ${this.name}, email: ${this.email})`;
+        }
+    }
+
+    class Teacher extends Person {
+        constructor(name, email, subject) {
+            super(name, email);
+            this.subject = subject;
+        }
+        toString(){
+            return (super.toString()).slice(0, -1) + `, subject: ${this.subject})`;
+        }
+    }
+
+    class Student extends Person {
+        constructor(name, email, course) {
+            super(name, email);
+            this.course = course;
+        }
+        toString(){
+            return (super.toString()).slice(0, -1) + `, course: ${this.course})`;
+        }
+    }
+
+    return {
+        Person,
+        Teacher,
+        Student
+    }
+}
+
+let classes = toStringExtension();
+let Person = classes.Person;
+let Teacher = classes.Teacher;
+let Student = classes.Student;
+
+let p = new Person("Pesho", 'Pesho@pesh.com');
+let t = new Teacher('Mima', 'belka@abv.bg', 'Graphics');
+console.log(p.toString() == 'Person (name: Pesho, email: Pesho@pesh.com)');
+console.log(t.toString());
