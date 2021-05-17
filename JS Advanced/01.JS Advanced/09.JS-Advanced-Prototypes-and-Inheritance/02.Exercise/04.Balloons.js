c921f234f74e8f11a3807bb58590b8ebc918940a
Videnoff <ivan.videnov@hotmail.com>
2021-05-17 20:21:45 +0000

    Add files via upload

---
+++ JS Advanced/01.JS Advanced/09.JS-Advanced-Prototypes-and-Inheritance/02.Exercise/04.Balloons.js
@@ -0,0 +1,48 @@
+function solve() {
+    class Balloon {
+        constructor(color, gasWeight) {
+            this.color = color;
+            this.gasWeight = gasWeight;
+        }
+    }
+
+    class PartyBalloon extends Balloon {
+        constructor(color, gasWeight, ribbonColor, ribbonLength) {
+            super(color, gasWeight);
+            this._ribbon = {
+                color: ribbonColor,
+                length: ribbonLength
+            }
+        }
+
+        get ribbon() {
+            return this._ribbon;
+        }
+    }
+
+    class BirthdayBalloon extends PartyBalloon {
+        constructor(color, gasWeight, ribbonColor, ribbonLength, text) {
+            super(color, gasWeight, ribbonColor, ribbonLength);
+            this._text = text;
+        }
+
+        get text() {
+            return this._text;
+        }
+    }
+
+    return {
+        Balloon: Balloon,
+        PartyBalloon: PartyBalloon,
+        BirthdayBalloon: BirthdayBalloon
+    }
+}
+
+let classes = solve();
+let testBalloon = new classes.Balloon("Tumno-bqlo", 20.5);
+console.log(testBalloon.color);
+
+let testPartyBalloon = new classes.PartyBalloon("Tumno-bqlo", 20.5, "Svetlo-cherno", 10.25);
+console.log(testPartyBalloon.ribbon);
+
+let ribbon = testPartyBalloon.ribbon;
