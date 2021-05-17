d7acd424aa040d7078eb744955f3a46874cd0948
Videnoff <ivan.videnov@hotmail.com>
2021-05-17 20:23:07 +0000

    Add files via upload

---
+++ JS Advanced/01.JS Advanced/09.JS-Advanced-Prototypes-and-Inheritance/02.Exercise/06.Posts.js
@@ -0,0 +1,83 @@
+function solve() {
+    class Post {
+        constructor(title, content) {
+            this.title = title;
+            this.content = content;
+        }
+
+        toString() {
+            return `Post: ${this.title}\nContent: ${this.content}`
+        }
+    }
+
+    class SocialMediaPost extends Post {
+        constructor(title, content, likes, dislikes) {
+            super(title, content);
+            this.likes = likes;
+            this.dislikes = dislikes;
+            this.comments = [];
+        }
+
+        addComment(comment) {
+            this.comments.push(comment);
+        }
+
+        toString() {
+            let result =  super.toString();
+            let rating = `Rating: ${this.likes - this.dislikes}`;
+            let comments = this.comments.length > 0 ?  `Comments:\n${this.comments.map(comment=> `  * ${comment}`).join('\n')}` : '';
+
+            return [
+                result,
+                rating,
+                comments].join('\n').trim();
+        }
+    }
+
+
+    class BlogPost extends Post {
+        constructor(title, content, views) {
+            super(title, content);
+            this.views = views;
+        }
+
+        view() {
+            this.views++;
+            return this;
+        }
+
+        toString() {
+            return super.toString() + `\nViews: ${this.views}`;
+        }
+    }
+
+    return {
+        Post,
+        SocialMediaPost,
+        BlogPost
+    }
+}
+
+const classes = solve();
+let post = new classes.Post("Post", "Content");
+
+console.log(post.toString());
+
+// Post: Post
+// Content: Content
+
+let scm = new classes.SocialMediaPost("TestTitle", "TestContent", 25, 30);
+
+scm.addComment("Good post");
+scm.addComment("Very good post");
+scm.addComment("Wow!");
+
+console.log(scm.toString());
+
+// Post: TestTitle
+// Content: TestContent
+// Rating: -5
+// Comments:
+//  * Good post
+//  * Very good post
+//  * Wow!
