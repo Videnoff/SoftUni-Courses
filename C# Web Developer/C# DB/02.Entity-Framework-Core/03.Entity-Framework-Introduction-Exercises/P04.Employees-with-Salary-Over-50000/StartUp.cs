a2751a4a8c5649381a9cab9607127db812c1f4bb
Videnoff <ivan.videnov@hotmail.com>
2021-06-22 23:44:48 +0300

    Create StartUp.cs

---
+++ C# Web Developer/C# DB/02.Entity-Framework-Core/03.Entity-Framework-Introduction-Exercises/P04.Employees-with-Salary-Over-50000/StartUp.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Linq;
+using System.Text;
+using SoftUni.Data;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+
+            string result = GetEmployeesWithSalaryOver50000(context);
+
+            Console.WriteLine(result);
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees
+                .Where(e => e.Salary > 50000)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.Salary
+                })
+                .OrderBy(e => e.FirstName)
+                .ToList();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} - {employee.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
