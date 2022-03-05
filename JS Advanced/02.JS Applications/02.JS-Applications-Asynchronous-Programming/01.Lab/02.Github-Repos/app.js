ec68cdd510e1d1703b482def3d3d2e589ee7bd60
Videnoff <ivan.videnov@hotmail.com>
2022-03-05 21:05:18 +0000

    Add files via upload

---
+++ JS Advanced/02.JS Applications/02.JS-Applications-Asynchronous-Programming/01.Lab/02.Github-Repos/app.js
@@ -0,0 +1,16 @@
+function loadRepos() {
+    const username = document.getElementById('username').value;
+    const url = `https://api.github.com/users/${username}/repos`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const ulElement = document.getElementById('repos');
+            ulElement.innerHTML = '';
+            data.forEach(r => {
+                const liElement = document.createElement('li');
+                liElement.textContent = r.full_name;
+                ulElement.appendChild(liElement);
+            });
+        });
+}
