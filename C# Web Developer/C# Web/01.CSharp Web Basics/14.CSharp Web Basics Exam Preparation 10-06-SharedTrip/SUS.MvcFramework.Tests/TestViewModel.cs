8e6ea2e9425a7e5dd5cb0135992fa2f7500761c5
Videnoff <ivan.videnov@hotmail.com>
2022-04-28 14:07:12 +0300

    Create TestViewModel.cs

---
+++ C# Web Developer/C# Web/01.CSharp Web Basics/14.CSharp Web Basics Exam Preparation 10-06-SharedTrip/SUS.MvcFramework.Tests/TestViewModel.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace SUS.MvcFramework.Tests
+{
+    public class TestViewModel
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+    }
+}
