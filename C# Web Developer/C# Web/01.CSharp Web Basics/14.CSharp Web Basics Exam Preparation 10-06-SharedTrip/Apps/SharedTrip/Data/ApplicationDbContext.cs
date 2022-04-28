ddf61fd2a174a0849c4516ddae45df47d72b17a5
Videnoff <ivan.videnov@hotmail.com>
2022-04-28 14:04:25 +0300

    Add files via upload

---
+++ C# Web Developer/C# Web/01.CSharp Web Basics/14.CSharp Web Basics Exam Preparation 10-06-SharedTrip/Apps/SharedTrip/Data/ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿namespace SharedTrip.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions db)
+            : base(db)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP-FSQK3RB;Database=SharedTrip;Integrated Security=true;");
+            }
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Trip> Trips { get; set; }
+
+        public DbSet<UserTrip> UserTrips { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserTrip>().HasKey(x => new
+            {
+                x.TripId,
+                x.UserId
+            });
+        }
+    }
+}
---
+++ C# Web Developer/C# Web/01.CSharp Web Basics/14.CSharp Web Basics Exam Preparation 10-06-SharedTrip/Apps/SharedTrip/Services/IUsersService.cs
@@ -0,0 +1,13 @@
+﻿namespace SharedTrip.Services
+{
+    public interface IUsersService
+    {
+        string GetUserId(string username, string password);
+
+        void Create(string username, string email, string password);
+
+        bool IsEmailAvailable(string email);
+
+        bool IsUsernameAvailable(string username);
+    }
+}
