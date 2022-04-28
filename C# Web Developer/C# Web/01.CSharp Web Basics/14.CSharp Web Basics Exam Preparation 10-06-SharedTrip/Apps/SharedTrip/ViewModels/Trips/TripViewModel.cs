3c500319eaca408551621a7cd370f602e28ad7c9
Videnoff <ivan.videnov@hotmail.com>
2022-04-28 14:04:52 +0300

    Add files via upload

---
+++ C# Web Developer/C# Web/01.CSharp Web Basics/14.CSharp Web Basics Exam Preparation 10-06-SharedTrip/Apps/SharedTrip/ViewModels/Trips/TripViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SharedTrip.ViewModels.Trips
+{
+    public class TripViewModel
+    {
+        public string Id { get; set; }
+        public string StartPoint { get; set; }
+
+        public string EndPoint { get; set; }
+
+        public DateTime DepartureTime { get; set; }
+
+        public int Seats { get; set; }
+
+        public int UsedSeats { get; set; }
+
+        public int AvailableSeats => this.Seats - this.UsedSeats;
+    }
+}
