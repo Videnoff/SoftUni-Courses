697e773180469bc062ccc306b6f221cb833b1713
Videnoff <ivan.videnov@hotmail.com>
2022-05-22 21:03:07 +0300

    Create StartUp.cs

---
+++ C# Web Developer/C# Web/01.CSharp Web Basics/15.CSharp Web Basics Exam 23-12-CarShop/CarShop/StartUp.cs
@@ -0,0 +1,30 @@
+namespace CarShop
+{
+    using BasicWebServer.Server;
+    using BasicWebServer.Server.Routing;
+    using CarShop.Contracts;
+    using CarShop.Data;
+    using CarShop.Data.Common;
+    using CarShop.Services;
+    using System.Threading.Tasks;
+
+    public class Startup
+    {
+        public static async Task Main()
+        {
+            var server = new HttpServer(routes => routes
+               .MapControllers()
+               .MapStaticFiles());
+
+            server.ServiceCollection
+                .Add<IUserService, UserService>()
+                .Add<ICarService, CarService>()
+                .Add<IIssueService, IssueService>()
+                .Add<ApplicationDbContext>()
+                .Add<IRepository, Repository>()
+                .Add<IValidationService, ValidationService>();
+
+            await server.Start();
+        }
+    }
+}
