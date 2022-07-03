8f58b35d126372b8388868835e5de3fddfe84e57
Videnoff <ivan.videnov@hotmail.com>
2022-07-03 23:36:31 +0300

    Add files via upload

---
+++ Data-Structures-Fundamentals/02.Linear-Data-Structures/01.Lab/LinearDataStructures-Lab/ImplementLinkedList/Node.cs
@@ -0,0 +1,13 @@
+namespace ImplementLinkedList;
+
+public class Node<T>
+{
+    public Node(T value)
+    {
+        Value = value;
+    }
+
+    public T Value { get; set; }
+
+    public Node<T>  Next { get; set; }
+}
