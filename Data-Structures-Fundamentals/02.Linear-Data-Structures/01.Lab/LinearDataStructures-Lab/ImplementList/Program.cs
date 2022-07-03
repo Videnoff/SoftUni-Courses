0a48991ae811d0a11b9954f3bc5d30d0d27bf3d6
Videnoff <ivan.videnov@hotmail.com>
2022-07-03 20:37:15 +0000

    Create Program.cs

---
+++ Data-Structures-Fundamentals/02.Linear-Data-Structures/01.Lab/LinearDataStructures-Lab/ImplementList/Program.cs
@@ -0,0 +1,17 @@
+namespace ImplementList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CoolList<int> list = new CoolList<int>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                list.Add(i * 5);
+                Console.WriteLine($"Internal array count: {list.InternalArrayCount}");
+                Console.WriteLine($"List count: {list.Count}");
+            }
+        }
+    }
+}
