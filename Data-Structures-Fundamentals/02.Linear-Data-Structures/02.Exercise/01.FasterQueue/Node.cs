aca1a1040ecbdfb1dadd8dd4c983a55e31f987b8
Videnoff <ivan.videnov@hotmail.com>
2022-07-05 13:04:17 +0300

    Add files via upload

---
+++ Data-Structures-Fundamentals/02.Linear-Data-Structures/02.Exercise/01.FasterQueue/Node.cs
@@ -0,0 +1,19 @@
+﻿namespace Problem01.FasterQueue
+{
+    public class Node<T>
+    {
+        public Node()
+        {
+        }
+
+        public Node(T item, Node<T> next)
+        {
+            this.Next = next;
+            this.Item = item;
+        }
+
+        public T Item { get; set; }
+
+        public Node<T> Next { get; set; }
+    }
+}
