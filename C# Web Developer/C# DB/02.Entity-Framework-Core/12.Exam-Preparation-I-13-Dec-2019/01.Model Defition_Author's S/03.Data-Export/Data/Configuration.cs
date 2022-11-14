9c00e00f4a8ad400de3c9a3578d63ab8a131509f
Videnoff <ivan.videnov@hotmail.com>
2022-11-14 23:03:52 +0200

    Create Configuration.cs

---
+++ C# Web Developer/C# DB/02.Entity-Framework-Core/12.Exam-Preparation-I-13-Dec-2019/01.Model Defition_Author's S/03.Data-Export/Data/Configuration.cs
@@ -0,0 +1,7 @@
+namespace BookShop.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=DESKTOP-F0IMF0D\SQLEXPRESS;Database=BookShop;Trusted_Connection=True";
+    }
+}
