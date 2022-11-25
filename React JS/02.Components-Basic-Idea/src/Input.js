673edc06eaeb1275f26126c90c56fe8550e75ddf
Videnoff <ivan.videnov@hotmail.com>
2022-11-25 20:27:01 +0200

    Add files via upload

---
+++ React JS/02.Components-Basic-Idea/src/Input.js
@@ -0,0 +1,30 @@
+import React, {Component} from "react";
+
+class Input extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            value: '',
+        }
+    }
+
+    changeValue = (event) => {
+        this.setState({
+            value: event.target.value,
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <input onChange={this.changeValue}/>
+                <p>
+                    {this.state.value}
+                </p>
+            </div>)
+    }
+
+}
+
+export default Input
