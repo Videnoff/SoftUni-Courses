6314961db5cd7f18d5f1136d9e1fc6880d49e5aa
Videnoff <ivan.videnov@hotmail.com>
2022-12-03 23:24:14 +0200

    Add files via upload

---
+++ React JS/04.Workshop-Components/origami/REST API/config/config.js
@@ -0,0 +1,12 @@
+const env = process.env.NODE_ENV || 'development';
+
+const config = {
+    development: {
+        port: process.env.PORT || 9999,
+        dbURL: 'mongodb+srv://baigergi:ZiGdffSS6EhUED3Czxeb@cluster0.2jhj82v.mongodb.net/origami?retryWrites=true&w=majority',
+        authCookieName: 'x-auth-token'
+    },
+    production: {}
+};
+
+module.exports = config[env];
---
+++ React JS/04.Workshop-Components/origami/REST API/config/express.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const secret = 'secret';
+
+module.exports = (app) => {
+    app.use(cors({
+        exposedHeaders: 'Authorization'
+    }));
+
+    app.use(express.json())
+    app.use(express.urlencoded({
+        extended: true
+    }));
+
+    app.use(cookieParser(secret));
+};
