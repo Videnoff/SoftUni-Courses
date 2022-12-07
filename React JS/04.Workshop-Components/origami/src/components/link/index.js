689377fa621907577f7aee3325c9882eb415078f
Videnoff <ivan.videnov@hotmail.com>
2022-12-07 11:26:07 +0200

    Add files via upload

---
+++ React JS/04.Workshop-Components/origami/src/components/link/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from './index.module.css';
+const Link = ({title, href, type}) => {
+    return (
+        <div className={styles[`${type}-list-item`]}>
+            <a href={href} className={styles[`${type}-link`]}>
+                {title}
+            </a>
+        </div>
+    );
+}
+
+export default Link;
