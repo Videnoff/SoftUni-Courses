389152763951f86e88e47938d5583561c18166d1
Videnoff <ivan.videnov@hotmail.com>
2022-12-07 11:25:31 +0200

    Create index.js

---
+++ React JS/04.Workshop-Components/origami/src/components/header/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from './index.module.css';
+import Link from "../link";
+import logo from "../../images/white-origami-bird.png";
+
+const Header = () => {
+    return (
+        <header className={styles.navigation}>
+            <img className={styles.logo} src={logo}/>
+            <Link href="#" title="Going to 1" type="header"/>
+            <Link href="#" title="Going to 2" type="header"/>
+            <Link href="#" title="Going to 3" type="header"/>
+            <Link href="#" title="Going to 4" type="header"/>
+            <Link href="#" title="Going to 5" type="header"/>
+            <Link href="#" title="Going to 6" type="header"/>
+        </header>
+    );
+}
+
+export default Header;
