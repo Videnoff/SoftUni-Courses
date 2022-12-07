76de953a487fc34e51c2404d80a0ae0975cae3eb
Videnoff <ivan.videnov@hotmail.com>
2022-12-07 11:26:41 +0200

    Add files via upload

---
+++ React JS/04.Workshop-Components/origami/src/components/origami/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./index.module.css";
+import image from "../../images/blue-origami-bird.png";
+
+const Origami = ({description, author}) => {
+    return (
+        <div className={styles.container}>
+            <img className={styles.image} src={image}/>
+            <p className={styles.description}>
+                {description}
+            </p>
+            <div>
+            <span className={styles.user}>
+                <small>
+                    Author:
+                </small>
+                {author.username}
+            </span>
+            </div>
+        </div>);
+}
+
+export default Origami;
