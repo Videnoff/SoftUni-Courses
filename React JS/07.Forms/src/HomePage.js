e0dcf607cc055353122bc97e0c58182c04947552
Videnoff <ivan.videnov@hotmail.com>
2022-12-17 22:39:02 +0200

    Create HomePage.js

---
+++ React JS/07.Forms/src/HomePage.js
@@ -0,0 +1,30 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+import {Link} from "react-router-dom";
+
+function HomePage(props) {
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Home Page
+                </p>
+                <Link to="/search">
+                    Go to search
+                </Link>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default HomePage;
