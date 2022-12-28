47c1d64552da5ceff322bf547ee6ae3cd22b7939
Videnoff <ivan.videnov@hotmail.com>
2022-12-28 22:15:58 +0200

    Create App.js

---
+++ React JS/08.Workshop-Forms/origami-part-3/src/App.js
@@ -0,0 +1,91 @@
+import React, {Component} from "react";
+import UserContext from "./Context";
+
+function getCookie(name) {
+    const cookieValue = document.cookie.match('(^|;) ? ' + name + '=([^;]*)(;|$)');
+    return cookieValue ? cookieValue[2] : null;
+}
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loggedIn: null,
+            user: null,
+        }
+    }
+
+    logIn = (user) => {
+        this.setState({
+            loggedIn: true,
+            user,
+        });
+    }
+    logOut = (user) => {
+        document.cookie = "x-auth-token= ; expires = Thu, 01 Jan 1970 00:00:00 GMT"
+        this.setState({
+            loggedIn: false,
+            user: null,
+        });
+    }
+
+    componentDidMount() {
+        const token = getCookie('x-auth-token');
+        console.log(token);
+
+        if (!token) {
+            this.logOut();
+            return;
+        }
+
+        fetch('http://localhost9999/api/user/verify', {
+            method: 'POST',
+            body: JSON.stringify({
+                token
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(promise => {
+            console.log(promise);
+            return promise.json();
+        }).then(response => {
+            if (response.status) {
+                this.logIn({
+                    username: response.user.username,
+                    id: response.user._id,
+                });
+            } else {
+                this.logOut();
+            }
+        });
+    }
+
+    render() {
+        const {
+            loggedIn,
+            user
+        } = this.state;
+
+        if (loggedIn === null) {
+            return (
+                <div>
+                    Loading...
+                </div>
+            )
+        }
+
+        return (
+            <UserContext.Provider value={{
+                loggedIn,
+                user,
+                logIn: this.logIn,
+                logOut: this.logOut,
+            }}>
+                {this.props.children};
+            </UserContext.Provider>
+        )
+    }
+}
+
+export default App;
