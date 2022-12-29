bf6692fe0c3dd1417feac5896d19b3433ca6fbd7
Videnoff <ivan.videnov@hotmail.com>
2022-12-29 22:16:56 +0200

    Create index.js

---
+++ React JS/08.Workshop-Forms/origami-part-3/src/components/aside/index.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react'
+import Link from '../link'
+import styles from './index.module.css'
+import getNavigation from '../../utils/navigation'
+import UserContext from "../../Context";
+
+class Aside extends Component {
+
+    static contextType = UserContext;
+
+    render() {
+
+        const {
+            loggedIn,
+            user
+        } = this.context;
+
+        const links = getNavigation(loggedIn, user);
+
+        return (
+            <aside className={styles.container}>
+                {
+                    links.map(navElement => {
+                        return (
+                            <Link
+                                key={navElement.title}
+                                href={navElement.link}
+                                title={navElement.title}
+                                type="aside"
+                            />
+                        )
+                    })
+                }
+            </aside>
+        )
+    }
+}
+
+export default Aside
