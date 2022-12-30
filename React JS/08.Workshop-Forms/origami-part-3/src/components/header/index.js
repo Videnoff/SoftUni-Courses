c28c90bd9868e34a693aa1d2e8c4060f709fae50
Videnoff <ivan.videnov@hotmail.com>
2022-12-30 19:28:08 +0200

    Add files via upload

---
+++ React JS/08.Workshop-Forms/origami-part-3/src/components/header/index.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react'
+import Link from '../link'
+import styles from './index.module.css'
+import logo from '../../images/white-origami-bird.png'
+import getNavigation from '../../utils/navigation'
+import UserContext from "../../Context";
+
+
+class Header extends Component {
+
+    static contextType = UserContext;
+
+    render() {
+
+        const {
+            loggedIn,
+            user
+        } = this.context;
+
+        const links = getNavigation(loggedIn, user);
+
+        return (
+            <header className={styles.navigation}>
+                <img className={styles.logo} src={logo}/>
+                {
+                    links.map(navElement => {
+                        return (
+                            <Link
+                                key={navElement.title}
+                                href={navElement.link}
+                                title={navElement.title}
+                                type="header"
+                            />
+                        )
+                    })
+                }
+            </header>
+        )
+    }
+}
+
+
+export default Header
