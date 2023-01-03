45c0cc4b6162d5f24d13cf98b68aa12ad4221ea7
Videnoff <ivan.videnov@hotmail.com>
2023-01-03 12:55:43 +0200

    Add files via upload

---
+++ React JS/08.Workshop-Forms/origami-part-3/src/pages/publications/index.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+import PageLayout from '../../components/page-layout'
+import styles from './index.module.css'
+import Title from '../../components/title'
+import Origamis from '../../components/origamis'
+import UserContext from "../../Context";
+
+class Publications extends Component {
+    static contextType = UserContext;
+
+    render() {
+        console.log(this.context);
+
+        return (
+            <PageLayout>
+                <Title title="Publications"/>
+                <Origamis/>
+            </PageLayout>
+        )
+    }
+}
+
+export default Publications
