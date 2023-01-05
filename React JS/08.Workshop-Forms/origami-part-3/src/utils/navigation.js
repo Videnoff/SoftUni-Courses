014bf13a2f3a63a3275182cfaa261157f67e60b4
Videnoff <ivan.videnov@hotmail.com>
2023-01-05 12:52:30 +0200

    Add files via upload

---
+++ React JS/08.Workshop-Forms/origami-part-3/src/utils/navigation.js
@@ -0,0 +1,36 @@
+const getNavigation = (loggedIn, user) => {
+
+  const authLinks = [
+    {
+      title: "Publications",
+      link: "/"
+    },
+    {
+      title: "Share your thoughts",
+      link: "/share"
+    },
+    {
+      title: "Profile",
+      link: `/profile/${user && user.id}`
+    }
+  ]
+
+  const guestLinks = [
+    {
+      title: "Publications",
+      link: "/"
+    },
+    {
+      title: "Register",
+      link: "/register"
+    },
+    {
+      title: "Login",
+      link: "/login"
+    }
+  ]
+
+  return loggedIn ? authLinks : guestLinks;
+}
+
+export default getNavigation
