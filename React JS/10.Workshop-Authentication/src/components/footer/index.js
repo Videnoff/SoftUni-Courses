f8306b354e8b14674f94d536fc1545a9353df3e5
Videnoff <ivan.videnov@hotmail.com>
2023-02-03 22:27:42 +0200

    Add files via upload

---
+++ React JS/10.Workshop-Authentication/src/components/footer/index.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react'
+import Link from '../link'
+import styles from './index.module.css'
+import getNavigation from '../../utils/navigation'
+import UserContext from "../../Context";
+
+class Footer extends Component {
+
+    static contextType = UserContext;
+
+    render() {
+
+        const {
+            user
+        } = this.context;
+
+        const links = getNavigation(user);
+        return (
+            <footer className={styles.footer}>
+                <div>
+                    {
+                        links.map(navElement => {
+                            return (
+                                <Link
+                                    key={navElement.title}
+                                    href={navElement.link}
+                                    title={navElement.title}
+                                    type="footer"
+                                />
+                            )
+                        })
+                    }
+                </div>
+                <p className={styles.university}>
+                    Software University 2022
+                </p>
+
+            </footer>
+        )
+    }
+}
+
+export default Footer
