19ce84da892719c75008bee4de5d6a8425d33aac
Videnoff <ivan.videnov@hotmail.com>
2023-02-05 10:28:29 +0200

    Add files via upload

---
+++ React JS/10.Workshop-Authentication/src/components/origamis/index.js
@@ -0,0 +1,35 @@
+import React, {Component, useCallback, useContext, useEffect, useMemo, useState} from 'react'
+import styles from './index.module.css'
+import Origam from '../origam'
+import getOrigami from "../../utils/origami";
+import UserContext from "../../Context";
+
+const Origamis = (props) => {
+    const context = useContext(UserContext);
+    const [origamis, setOrigamis] = useState(context.origamis || []);
+
+    const getOrigamis = useCallback(async () => {
+        const origamis = await getOrigami(props.length);
+        setOrigamis(origamis);
+    }, [props.length]);
+
+    const renderOrigamis = useMemo(() => {
+        return origamis.map((origam, index) => {
+            return (
+                <Origam key={origam._id} index={index} {...origam} />
+            )
+        })
+    }, [origamis])
+
+    useEffect(() => {
+        getOrigamis();
+    }, [props.updatedOrigami, getOrigamis]);
+
+    return (
+        <div className={styles["origamis-wrapper"]}>
+            {renderOrigamis}
+        </div>
+    )
+}
+
+export default Origamis
