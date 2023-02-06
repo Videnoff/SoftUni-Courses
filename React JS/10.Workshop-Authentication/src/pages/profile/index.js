70936938834caa24f4d5fceb7f215498268212a6
Videnoff <ivan.videnov@hotmail.com>
2023-02-06 21:59:50 +0200

    Add files via upload

---
+++ React JS/10.Workshop-Authentication/src/pages/profile/index.js
@@ -0,0 +1,60 @@
+import React, {useCallback, useContext, useEffect, useState} from 'react'
+import PageLayout from '../../components/page-layout'
+import Origamis from '../../components/origamis'
+import UserContext from "../../Context";
+import {useNavigate, useParams} from "react-router-dom";
+
+const ProfilePage = () => {
+
+    const [username, setUsername] = useState(null);
+    const [posts, setPosts] = useState(null);
+    const context = useContext(UserContext);
+    const params = useParams();
+    const history = useNavigate();
+
+    const logOut = () => {
+        context.logOut();
+        history('/');
+    }
+
+    const getData = useCallback(async () => {
+        const id = params.userid;
+        const response = await fetch(`http://localhost:9999/api/user?id=${id}`)
+
+        if (!response.ok) {
+            history('/error')
+        } else {
+            const user = await response.json();
+            setUsername(user.username);
+            setPosts(user.posts && user.posts.length);
+        }
+    }, [params.userid, history]);
+
+    useEffect(() => {
+        getData()
+    }, [getData]);
+
+    if (!username) {
+        return (
+            <PageLayout>
+                <div>Loading....</div>
+            </PageLayout>
+        )
+    }
+
+    return (
+        <PageLayout>
+            <div>
+                <p>User: {username}</p>
+                <p>Posts: {posts}</p>
+
+                <button onClick={logOut}>
+                    Logout
+                </button>
+            </div>
+            <Origamis length={3}/>
+        </PageLayout>
+    )
+}
+
+export default ProfilePage
