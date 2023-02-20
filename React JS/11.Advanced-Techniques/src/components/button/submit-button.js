2313a24a8f71ff831aeab432f049f6da87be0328
Videnoff <ivan.videnov@hotmail.com>
2023-02-20 13:27:20 +0200

    Create submit-button.js

---
+++ React JS/11.Advanced-Techniques/src/components/button/submit-button.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const SubmitButton = ({title, onClick}) => {
+    return (
+        <Button type="submit" onClick={onClick}>
+            {title}
+        </Button>
+    )
+}
+
+const Button = styled.button`
+  background: #234465;
+  color: #fff;
+  padding: 10px;
+  width: 100px;
+  border-radius: 5px;
+  display:  block;
+  margin:  0 auto;
+  border: none;
+  border:  2px solid white;
+  margin: 10px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #fff;
+    border: 2px solid #234465;
+    color: #234465;
+    font-style:  italic;
+  }
+`
+
+export default SubmitButton
