0f612adb96d27753c5acf47157196b46fd2cd3bd
Videnoff <ivan.videnov@hotmail.com>
2023-03-04 20:32:06 +0200

    Create index.js

---
+++ React JS/11.Advanced-Techniques/src/pages/publications/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PageLayout from '../../components/page-layout'
+import Title from '../../components/title'
+import Origamis from '../../components/origamis'
+
+const Publications = () => {
+    return (
+        <PageLayout>
+            <Title title="Publications"/>
+            <Origamis/>
+        </PageLayout>
+    )
+}
+
+export default Publications
