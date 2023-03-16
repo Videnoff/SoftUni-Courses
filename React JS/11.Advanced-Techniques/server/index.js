5f162d1a2a1bc08e3bc70887153e261b3efbcfc1
Videnoff <ivan.videnov@hotmail.com>
2023-03-16 20:33:16 +0200

    Create index.js

---
+++ React JS/11.Advanced-Techniques/server/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+
+// we'll talk about this in a minute:
+import serverRenderer from './middleware/renderer';
+import getOrigamis from "./middleware/origamis";
+import path from 'path';
+
+const PORT = 3000;
+
+// initialize the application and create the routes
+const app = express();
+const router = express.Router();
+
+router.use('^/$', auth, getOrigamis, serverRenderer);
+
+// other static resources should just be served as they are
+router.use(express.static(
+    path.resolve(__dirname, '..', 'build'),
+    { maxAge: '30d' },
+));
+
+// tell the app to use the above rules
+router.use('*', serverRenderer);
+app.use(router);
+
+// start the app
+app.listen(PORT, (error) => {
+    if (error) {
+        return console.log('something bad happened', error);
+    }
+
+    console.log("listening on " + PORT + "...");
+});
